416de534621035d160a4caa8edb924d860322b69
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-02-20 10:00:38 +0900

    abc085_c

---
+++ atcoder/abs/abc085_c/20270460.py
@@ -0,0 +1,11 @@
+from sys import exit
+
+n, y = map(int, input().split())
+
+for i in range(n + 1):
+    for j in range(n - i + 1):
+        k = n - i - j
+        if 10000 * i + 5000 * j + 1000 * k == y:
+            print(i, j, k)
+            exit(0)
+print("-1 -1 -1")
