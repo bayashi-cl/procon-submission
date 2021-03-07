20fb098572cecfd9a48b6b4bf1b8a9f1b71b5115
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-03-07 11:27:18 +0900

    abc194_e

---
+++ atcoder/abc194/abc194_e/20743917.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <boost/range/irange.hpp>
+using boost::irange;
+
+using ll = long long;
+using P = pair<ll, ll>;
+
+const int INF = (1 << 30) - 1;
+const int MOD = 1e9 + 7;
+const ll LINF = 1LL << 60;
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+    vector<int> A(N);
+    for (auto&& i : A) cin >> i;
+    vector<int> count(N);
+    for (int i : irange(M)) count[A[i]]++;
+
+    auto&& itr = find(count.begin(), count.end(), 0);
+    int mex = distance(count.begin(), itr);
+
+    for (int i = 0; i < N - M; i++) {
+        count[A[i]]--;
+        count[A[i + M]]++;
+        if (count[A[i]] == 0) mex = min(mex, A[i]);
+    }
+    cout << mex << endl;
+    return 0;
+}
