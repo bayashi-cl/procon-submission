c57bd06f2e21587035f79013d20bf79ae903ef61
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-03-27 22:59:40 +0900

    abc197_c

---
+++ atcoder/abc197/abc197_c/21326249.py
@@ -0,0 +1,53 @@
+# region template
+import sys
+from typing import Any, Callable
+from itertools import combinations
+
+
+sys.setrecursionlimit(10 ** 9)
+sinput: Callable[..., str] = sys.stdin.readline
+INF: float = float("inf")
+MOD: int = 10 ** 9 + 7
+# endregion
+
+
+def calc_or(li: list) -> int:
+    res = 0
+    for l in li:
+        res = res | l
+    return res
+
+
+def calc_xor(li: list) -> int:
+    res = li[0]
+    if len(li) == 1:
+        return res
+
+    for l in li[1:]:
+        res = res ^ l
+    return res
+
+
+def main() -> None:
+    N = int(sinput())
+    A = list(map(int, sinput().split()))
+    if N == 1:
+        print(A[0])
+        return
+    ans = calc_xor(A)
+
+    for i in range(2, N + 1):
+        for sep in combinations(range(1, N), i - 1):
+            before = 0
+            or_list = list()
+            for s in sep:
+                or_list.append(calc_or(A[before:s]))
+                before = s
+            or_list.append(calc_or(A[before:]))
+            ans = min(ans, calc_xor(or_list))
+
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
