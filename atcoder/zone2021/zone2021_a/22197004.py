7df0e62384d8b7496485fffe7c0a7266efc630fc
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-05-01 21:02:12 +0900

    zone2021_a

---
+++ atcoder/zone2021/zone2021_a/22197004.py
@@ -0,0 +1,22 @@
+# region template
+from __future__ import annotations
+
+import sys
+from typing import Any, Callable, Final
+
+
+sys.setrecursionlimit(10 ** 9)
+sinput: Callable[..., str] = sys.stdin.readline
+INF: Final[float] = float("inf")
+MOD: Final[int] = 10 ** 9 + 7
+# endregion
+
+
+def main() -> None:
+    s = sinput().strip()
+
+    print(s.count("ZONe"))
+
+
+if __name__ == "__main__":
+    main()
