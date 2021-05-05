813971a07eeb9f57dee314faa803c96b8ade6ce9
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-05-06 08:04:40 +0900

    typical90_c

---
+++ atcoder/typical90/typical90_c/22338562.cpp
@@ -0,0 +1,67 @@
+#pragma region template
+// clang-format off
+// #include <bits/stdc++.h>
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+
+#include <boost/range/irange.hpp>
+using boost::irange;
+
+#include <atcoder/modint>
+using mint = atcoder::modint1000000007;
+
+using ll = long long;
+
+const int INF = INT_MAX / 2;
+const ll LINF = LLONG_MAX / 2;
+const int MOD = 1e9 + 7;
+const int ASCII_0 = 48;
+
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+template<class T>void printvec(const vector<T>& v) { for (T i : v) cout << i << " "; cout << endl;}
+
+struct pre_exec {pre_exec() {cin.tie(nullptr); ios::sync_with_stdio(false); cout << fixed << setprecision(11) << boolalpha; }; } pre_exec_t;
+// clang-format on
+#pragma endregion
+
+int max_depth = 0;
+int far = -1;
+vector<vector<int>> graph(110000, vector<int>());
+
+void dfs(int depth, int now, int from) {
+    if (chmax(max_depth, depth)) far = now;
+    for (auto&& to : graph[now]) {
+        if (to == from) continue;
+        dfs(depth + 1, to, now);
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n - 1; i++) {
+        int a, b;
+        cin >> a >> b;
+        a--;
+        b--;
+        graph[a].push_back(b);
+        graph[b].push_back(a);
+    }
+    dfs(0, 0, -1);
+    dfs(0, far, -1);
+    cout << max_depth + 1 << endl;
+    return 0;
+}
