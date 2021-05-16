5eb0dcabc8928c1d92663a0bdafe644eb94fda1b
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-05-17 03:03:01 +0900

    abc186_b

---
+++ atcoder/abc186/abc186_b/22677345.py
@@ -0,0 +1,25 @@
+# region template
+import sys
+import typing
+
+import numpy as np
+
+sys.setrecursionlimit(10 ** 9)
+Vec = typing.List[int]
+VecVec = typing.List[Vec]
+sinput: typing.Callable[..., str] = sys.stdin.readline
+MOD: int = 1000000007
+INF: float = float("Inf")
+IINF: int = sys.maxsize
+# endregion
+
+
+def main() -> None:
+    h, w = map(int, sinput().split())
+    a = np.array(list(map(int, sys.stdin.read().split())))
+    ans = np.sum(a - np.min(a))
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
