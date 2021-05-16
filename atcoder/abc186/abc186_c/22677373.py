4811a1f1cb0d5b89fd607a4b360457efddc42820
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-05-17 03:07:07 +0900

    abc186_c

---
+++ atcoder/abc186/abc186_c/22677373.py
@@ -0,0 +1,26 @@
+# region template
+import sys
+import typing
+
+sys.setrecursionlimit(10 ** 9)
+Vec = typing.List[int]
+VecVec = typing.List[Vec]
+sinput: typing.Callable[..., str] = sys.stdin.readline
+MOD: int = 1000000007
+INF: float = float("Inf")
+IINF: int = sys.maxsize
+# endregion
+
+
+def main() -> None:
+    n = int(sinput())
+    ans = 0
+    for i in range(1, n + 1):
+        if "7" in str(i) or "7" in oct(i):
+            continue
+        ans += 1
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
