d769fb7bf9c37c6972938620407c5d56ee108d55
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-05-19 13:17:01 +0900

    abc183_a

---
+++ atcoder/abc183/abc183_a/22723961.py
@@ -0,0 +1,20 @@
+def main() -> None:
+    # region template
+    import sys
+    import typing
+
+    sys.setrecursionlimit(10 ** 6)
+    Vec = typing.List[int]  # noqa:F841
+    VecVec = typing.List[Vec]  # noqa:F841
+    sinput: typing.Callable[..., str] = sys.stdin.readline  # noqa:F841
+    MOD: int = 1000000007  # noqa:F841
+    INF: float = float("Inf")  # noqa:F841
+    IINF: int = 4611686018427387903  # noqa:F841
+    # endregion
+
+    x = int(sinput())
+    print(max(0, x))
+
+
+if __name__ == "__main__":
+    main()
