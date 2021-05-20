b984b2b880858e0777e56126fd139c1a8c5ac68d
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-05-21 04:49:08 +0900

    abc177_a

---
+++ atcoder/abc177/abc177_a/22756610.cpp
@@ -0,0 +1,53 @@
+#pragma region include
+#include <atcoder/modint>
+#include <boost/range/irange.hpp>
+// #include <bits/stdc++.h>
+// #include <cmath>
+#include <iomanip>
+#include <iostream>
+// #include <iterator>
+#include <limits>
+// #include <map>
+// #include <numeric>
+// #include <queue>
+// #include <set>
+// #include <stack>
+#include <vector>
+#pragma endregion
+
+namespace mine {
+#pragma region template
+// clang-format off
+using std::cin, std::cout, std::endl, std::string, boost::irange;
+using ll = long long int;
+using mint = atcoder::modint1000000007;
+template <class T> using Vec = std::vector<T>;
+template <class T> using VecVec = std::vector<std::vector<T>>;
+template <class T> bool chmax(T& a, const T& b) {if (a < b) {a = b; return 1;} return 0;}
+template <class T> bool chmin(T& a, const T& b) {if (b < a) {a = b; return 1;} return 0;}
+template <class T> void printvec(const Vec<T>& v) {for (T i : v) cout << i << " "; cout << endl;}
+void init() {cin.tie(nullptr); std::ios::sync_with_stdio(false); cout << std::fixed << std::setprecision(11) << std::boolalpha;}
+constexpr int INF = std::numeric_limits<int>::max() / 2;
+constexpr ll LINF = std::numeric_limits<ll>::max() / 2;
+constexpr int MOD = 1e9 + 7;
+// clang-format on
+#pragma endregion
+
+void main() {
+    int d, t, s;
+    cin >> d >> t >> s;
+
+    if (s * t >= d) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+}
+
+}  // namespace mine
+
+int main() {
+    mine::init();
+    mine::main();
+    return 0;
+}
