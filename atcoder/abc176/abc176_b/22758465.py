f32b5bacd84cebf4bedfad083b3de86262392f7a
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-05-21 10:42:16 +0900

    abc176_b

---
+++ atcoder/abc176/abc176_b/22758465.py
@@ -0,0 +1,24 @@
+def main() -> None:
+    # region template
+    import sys
+    import typing
+
+    sys.setrecursionlimit(10 ** 6)
+    Vec = typing.List[int]  # noqa:F841
+    VecVec = typing.List[Vec]  # noqa:F841
+    sinput: typing.Callable[..., str] = sys.stdin.readline  # noqa:F841
+    MOD: int = 1000000007  # noqa:F841
+    INF: float = float("Inf")  # noqa:F841
+    IINF: int = 4611686018427387903  # noqa:F841
+    # endregion
+
+    n = sinput().strip()
+    s = [int(ni) for ni in n]
+    if sum(s) % 9 == 0:
+        print("Yes")
+    else:
+        print("No")
+
+
+if __name__ == "__main__":
+    main()
