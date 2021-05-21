1a7e4ebe6c72440870b8e10337c4a31dfb7a182c
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-05-21 10:40:06 +0900

    abc176_a

---
+++ atcoder/abc176/abc176_a/22758451.py
@@ -0,0 +1,20 @@
+def main() -> None:
+    # region template
+    import sys
+    import typing
+
+    sys.setrecursionlimit(10 ** 6)
+    Vec = typing.List[int]  # noqa:F841
+    VecVec = typing.List[Vec]  # noqa:F841
+    sinput: typing.Callable[..., str] = sys.stdin.readline  # noqa:F841
+    MOD: int = 1000000007  # noqa:F841
+    INF: float = float("Inf")  # noqa:F841
+    IINF: int = 4611686018427387903  # noqa:F841
+    # endregion
+
+    n, x, t = map(int, sinput().split())
+    print(((n - 1) // x + 1) * t)
+
+
+if __name__ == "__main__":
+    main()
