e27e8c7506cd3dbb52f6e5595d93ad6869231846
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-05-23 05:25:27 +0900

    abc172_b

---
+++ atcoder/abc172/abc172_b/22847539.py
@@ -0,0 +1,26 @@
+def main() -> None:
+    # region template
+    import sys
+    import typing
+
+    sys.setrecursionlimit(10 ** 6)
+    Vec = typing.List[int]  # noqa:F841
+    VecVec = typing.List[Vec]  # noqa:F841
+    sinput: typing.Callable[..., str] = sys.stdin.readline  # noqa:F841
+    MOD: int = 1000000007  # noqa:F841
+    INF: float = float("Inf")  # noqa:F841
+    IINF: int = 4611686018427387903  # noqa:F841
+    # endregion
+
+    s = list(sinput().strip())
+    t = list(sinput().strip())
+
+    ans = 0
+    for si, ti in zip(s, t):
+        if si != ti:
+            ans += 1
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
