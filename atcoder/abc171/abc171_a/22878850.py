91514ddd825e7628138e5f5a785e9909b060e6e4
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-05-24 00:57:23 +0900

    abc171_a

---
+++ atcoder/abc171/abc171_a/22878850.py
@@ -0,0 +1,23 @@
+def main() -> None:
+    # region template
+    import sys
+    import typing
+
+    sys.setrecursionlimit(10 ** 6)
+    Vec = typing.List[int]  # noqa:F841
+    VecVec = typing.List[Vec]  # noqa:F841
+    sinput: typing.Callable[..., str] = sys.stdin.readline  # noqa:F841
+    MOD: int = 1000000007  # noqa:F841
+    INF: float = float("Inf")  # noqa:F841
+    IINF: int = 4611686018427387903  # noqa:F841
+    # endregion
+
+    alpha = sinput().strip()
+    if alpha.isupper():
+        print("A")
+    else:
+        print("a")
+
+
+if __name__ == "__main__":
+    main()
