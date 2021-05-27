db18b4a741abe4de57d73da0fc5be4ac633a4543
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-05-27 13:00:48 +0900

    abc160_a

---
+++ atcoder/abc160/abc160_a/22941967.py
@@ -0,0 +1,24 @@
+# region template
+import sys
+import typing
+
+sys.setrecursionlimit(10 ** 6)
+Vec = typing.List[int]
+VecVec = typing.List[Vec]
+sinput: typing.Callable[..., str] = sys.stdin.readline
+MOD: int = 1000000007
+INF: float = float("Inf")
+IINF: int = sys.maxsize
+# endregion
+
+
+def main() -> None:
+    s = sinput().strip()
+    if s[2] == s[3] and s[4] == s[5]:
+        print("Yes")
+    else:
+        print("No")
+
+
+if __name__ == "__main__":
+    main()
