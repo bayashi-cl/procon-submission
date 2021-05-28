ea032e83761976687922f27395edb8532cf49162
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-05-29 05:37:10 +0900

    typical90_ax

---
+++ atcoder/typical90/typical90_ax/22975280.py
@@ -0,0 +1,29 @@
+# region template
+import sys
+import typing
+
+sys.setrecursionlimit(10 ** 6)
+Vec = typing.List[int]
+VecVec = typing.List[Vec]
+sinput: typing.Callable[..., str] = sys.stdin.readline
+MOD: int = 1000000007
+INF: float = float("Inf")
+IINF: int = sys.maxsize
+# endregion
+
+
+def main() -> None:
+    n, l = map(int, sinput().split())
+    dp = [0] * (n + 1)
+    dp[0] = 1
+    for i in range(1, n + 1):
+        dp[i] = dp[i - 1]
+        if i - l >= 0:
+            dp[i] += dp[i - l]
+        dp[i] %= MOD
+
+    print(dp[-1])
+
+
+if __name__ == "__main__":
+    main()
