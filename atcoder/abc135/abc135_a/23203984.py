3824cdfa03ba01ef6155a4bbcb3d1961d875b29a
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-06-06 16:25:41 +0900

    abc135_a

---
+++ atcoder/abc135/abc135_a/23203984.py
@@ -0,0 +1,25 @@
+# region template
+import sys
+import typing
+
+sys.setrecursionlimit(10 ** 6)
+Vec = typing.List[int]
+VecVec = typing.List[Vec]
+sinput: typing.Callable[..., str] = sys.stdin.readline
+MOD: int = 1000000007
+INF: float = float("Inf")
+IINF: int = sys.maxsize
+# endregion
+
+
+def main() -> None:
+    a, b = map(int, sinput().split())
+    if (a + b) % 2 == 0:
+        print((a + b) // 2)
+
+    else:
+        print("IMPOSSIBLE")
+
+
+if __name__ == "__main__":
+    main()
