10596304bfa89430e8f974cadea6620080a0f031
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-06-06 21:02:43 +0900

    abc204_b

---
+++ atcoder/abc204/abc204_b/23213889.py
@@ -0,0 +1,26 @@
+# region template
+import sys
+import typing
+
+sys.setrecursionlimit(10 ** 6)
+Vec = typing.List[int]
+VecVec = typing.List[Vec]
+sinput: typing.Callable[..., str] = sys.stdin.readline
+MOD: int = 1000000007
+INF: float = float("Inf")
+IINF: int = sys.maxsize
+# endregion
+
+
+def main() -> None:
+    n = int(sinput())
+    a = list(map(int, sinput().split()))
+    ans = 0
+    for ai in a:
+        if ai > 10:
+            ans += ai - 10
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
