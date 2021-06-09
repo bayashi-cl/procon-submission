14fac11d3f4e8ba38ec94f4d3f97d58a103301df
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-06-09 21:03:13 +0900

    abc123_c

---
+++ atcoder/abc123/abc123_c/23312799.py
@@ -0,0 +1,23 @@
+# region template
+import sys
+import typing
+
+sys.setrecursionlimit(10 ** 6)
+Vec = typing.List[int]
+VecVec = typing.List[Vec]
+sinput: typing.Callable[..., str] = sys.stdin.readline
+MOD: int = 1000000007
+INF: float = float("Inf")
+IINF: int = sys.maxsize
+# endregion
+
+
+def main() -> None:
+    n, *t = map(int, sys.stdin.read().split())
+    mi = min(t)
+    ans = -(-n // mi) + 4
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
