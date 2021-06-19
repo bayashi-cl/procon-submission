33d4d90e62c2db0edbc80b1d68b2c51304c6fabd
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-06-19 21:06:10 +0900

    abc206_b

---
+++ atcoder/abc206/abc206_b/23579586.py
@@ -0,0 +1,52 @@
+# region template
+import sys
+import typing
+
+sys.setrecursionlimit(10 ** 6)
+Vec = typing.List[int]
+VecVec = typing.List[Vec]
+sinput: typing.Callable[..., str] = sys.stdin.readline
+MOD: int = 1000000007
+INF: float = float("Inf")
+IINF: int = sys.maxsize
+# endregion
+
+
+def meguru_bisect(ok: int, ng: int, is_ok: typing.Callable[..., bool], *args) -> int:
+    """二分探索法
+    Args:
+        ok (int): is_okを満たす初期値
+        ng (int): is_okを満たさない初期値
+        is_ok (typing.Callable[..., bool]): 判定用関数
+        *args (object): is_okに渡される引数
+
+    Returns:
+        int: is_okを満たす最大/小の整数
+    """
+    assert is_ok(ok, *args)
+    assert not is_ok(ng, *args)
+
+    while abs(ok - ng) > 1:
+        mid = (ok + ng) >> 1
+        if is_ok(mid, *args):
+            ok = mid
+        else:
+            ng = mid
+    return ok
+
+
+def main() -> None:
+    n = int(sinput())
+    ok = 10 ** 9
+    ng = 0
+
+    def is_ok(x):
+        # if x == 0:
+        #     return False
+        return x * (x + 1) // 2 >= n
+
+    print(meguru_bisect(ok, ng, is_ok))
+
+
+if __name__ == "__main__":
+    main()
