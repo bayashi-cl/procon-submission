99992d57261d66c9462403dc2324d63c9579e389
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-06-20 20:44:24 +0900

    C: Stable Groups

---
+++ codeforces/1539/C/120113328.cpp
@@ -0,0 +1,82 @@
+#pragma region template
+// #include <bits/stdc++.h>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <valarray>
+#include <vector>
+
+namespace mine {
+using std::cin, std::cout, std::endl;
+using std::min, std::max, std::sort, std::abs, std::pow;
+using std::vector, std::string, std::set, std::map;
+using ll = long long int;
+using Pa = std::pair<int, int>;
+using Vec = std::vector<int>;
+using VecVec = std::vector<Vec>;
+constexpr int MOD = 1000000007;
+constexpr int INF = std::numeric_limits<int>::max() / 2;
+constexpr ll LINF = std::numeric_limits<ll>::max() / 2;
+// clang-format off
+template <class T> inline bool chmax(T& a, const T& b) {if (a < b) {a = b; return 1;} return 0;}
+template <class T> inline bool chmin(T& a, const T& b) {if (b < a) {a = b; return 1;} return 0;}
+template <class T> std::istream& operator>>(std::istream& is, vector<T>& vec) {for (auto&& v : vec) is >> v; return is;}
+template <class T> std::ostream& operator<<(std::ostream& os, const vector<T>& vec) {for (std::size_t i = 0; i < vec.size(); i++) {if (i) os << " "; os << vec[i];} return os;}
+template <class T> std::istream& operator>>(std::istream& is, std::valarray<T>& arr) {for (auto&& a : arr) is >> a; return is;}
+template <class T> std::ostream& operator<<(std::ostream& os, const std::valarray<T>& arr) {for (std::size_t i = 0; i < arr.size(); i++) {if (i) os << " "; os << arr[i];} return os;}
+template <class S, class T> std::istream& operator>>(std::istream& is, std::pair<S, T>& p) {is >> p.first >> p.second; return is;}
+template <class S, class T> std::ostream& operator<<(std::ostream& os, const std::pair<S, T>& p) {os << p.first << " " << p.second; return os;}
+template <class Tail> void print(Tail&& tail) {cout << tail << "\n";}
+template <class Head, class... Body> void print(Head&& head, Body&&... tail) {cout << head << " "; print(std::forward<Body>(tail)...);}
+template <class Tail> void debug(Tail&& tail) {std::clog << tail << endl;}
+template <class Head, class... Body> void debug(Head&& head, Body&&... tail) {std::clog << head << " "; debug(std::forward<Body>(tail)...);}
+inline void init() {cin.tie(nullptr); std::ios::sync_with_stdio(false); cout << std::fixed << std::setprecision(11) << std::boolalpha;}
+// clang-format on
+#pragma endregion
+
+/*
+n人の生徒を"安定した"グループに分ける
+各生徒のレベルはai
+"安定した": ソートしたとき隣り合う生徒のレベルの差がx以下
+教師は任意のレベルの生徒をk人まで追加できる
+グループの最小数
+*/
+
+void main() {
+    ll n, k, x;
+    cin >> n >> k >> x;
+    vector<ll> a(n);
+    cin >> a;
+
+    sort(std::begin(a), std::end(a));
+    vector<ll> diff;
+    for (int i = 0; i < n - 1; i++) {
+        if (a[i + 1] - a[i] > x) diff.push_back(a[i + 1] - a[i]);
+    }
+    sort(std::begin(diff), std::end(diff));
+    for (auto&& d : diff) d = (d + x - 1) / x - 1;
+    int ans = diff.size() + 1;
+    for (auto&& d : diff) {
+        if (k < d) break;
+        ans--;
+        k -= d;
+    }
+    print(ans);
+}
+
+}  // namespace mine
+
+int main() {
+    mine::init();
+    mine::main();
+    std::cout << std::flush;
+    return 0;
+}
