c9ac62502c7549fb0c267b1e766825df68912ff7
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-06-20 20:08:35 +0900

    B: Love Song

---
+++ codeforces/1539/B/120096343.cpp
@@ -0,0 +1,72 @@
+#pragma region template
+// #include <bits/stdc++.h>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <valarray>
+#include <vector>
+
+namespace mine {
+using std::cin, std::cout, std::endl;
+using std::min, std::max, std::sort, std::abs, std::pow;
+using std::vector, std::string, std::set, std::map;
+using ll = long long int;
+using Pa = std::pair<int, int>;
+using Vec = std::vector<int>;
+using VecVec = std::vector<Vec>;
+constexpr int MOD = 1000000007;
+constexpr int INF = std::numeric_limits<int>::max() / 2;
+constexpr ll LINF = std::numeric_limits<ll>::max() / 2;
+// clang-format off
+template <class T> inline bool chmax(T& a, const T& b) {if (a < b) {a = b; return 1;} return 0;}
+template <class T> inline bool chmin(T& a, const T& b) {if (b < a) {a = b; return 1;} return 0;}
+template <class T> std::istream& operator>>(std::istream& is, vector<T>& vec) {for (auto&& v : vec) is >> v; return is;}
+template <class T> std::ostream& operator<<(std::ostream& os, const vector<T>& vec) {for (std::size_t i = 0; i < vec.size(); i++) {if (i) os << " "; os << vec[i];} return os;}
+template <class T> std::istream& operator>>(std::istream& is, std::valarray<T>& arr) {for (auto&& a : arr) is >> a; return is;}
+template <class T> std::ostream& operator<<(std::ostream& os, const std::valarray<T>& arr) {for (std::size_t i = 0; i < arr.size(); i++) {if (i) os << " "; os << arr[i];} return os;}
+template <class S, class T> std::istream& operator>>(std::istream& is, std::pair<S, T>& p) {is >> p.first >> p.second; return is;}
+template <class S, class T> std::ostream& operator<<(std::ostream& os, const std::pair<S, T>& p) {os << p.first << " " << p.second; return os;}
+template <class Tail> void print(Tail&& tail) {cout << tail << "\n";}
+template <class Head, class... Body> void print(Head&& head, Body&&... tail) {cout << head << " "; print(std::forward<Body>(tail)...);}
+template <class Tail> void debug(Tail&& tail) {std::clog << tail << endl;}
+template <class Head, class... Body> void debug(Head&& head, Body&&... tail) {std::clog << head << " "; debug(std::forward<Body>(tail)...);}
+inline void init() {cin.tie(nullptr); std::ios::sync_with_stdio(false); cout << std::fixed << std::setprecision(11) << std::boolalpha;}
+// clang-format on
+#pragma endregion
+
+void main() {
+    int n, q;
+    cin >> n >> q;
+    string s;
+    cin >> s;
+    VecVec cnt(27, Vec(n + 1));
+    for (int i = 1; i <= n; i++) {
+        int idx = (int)s[i - 1] - (int)'a' + 1;
+        cnt[idx][i]++;
+        for (int j = 1; j <= 26; j++) cnt[j][i] += cnt[j][i - 1];
+    }
+    for (int que = 0; que < q; que++) {
+        ll ans = 0;
+        int l, r;
+        cin >> l >> r;
+        for (int i = 1; i <= 26; i++) ans += (cnt[i][r] - cnt[i][l - 1]) * i;
+        print(ans);
+    }
+}
+
+}  // namespace mine
+
+int main() {
+    mine::init();
+    mine::main();
+    std::cout << std::flush;
+    return 0;
+}
