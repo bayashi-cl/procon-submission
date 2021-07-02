0f182743047c8b8cb17207bf47cc8b61130a2b6c
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-07-02 17:38:52 +0900

    abc070_a

---
+++ atcoder/abc070/abc070_a/23909625.py
@@ -0,0 +1,25 @@
+# region template
+import sys
+import typing
+from typing import Callable, Dict, List, Set, Tuple
+
+sys.setrecursionlimit(10 ** 6)
+Vec = List[int]
+VecVec = List[Vec]
+sinput: Callable[..., str] = sys.stdin.readline
+MOD: int = 1000000007
+INF: float = float("Inf")
+IINF: int = sys.maxsize
+# endregion
+
+
+def main() -> None:
+    n = sinput().strip()
+    if n[0] == n[2]:
+        print("Yes")
+    else:
+        print("No")
+
+
+if __name__ == "__main__":
+    main()
