89c7424fc7fdf47582db362252af0cbe81e38b52
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-07-09 20:27:02 +0900

    arc069_b

---
+++ atcoder/abc055/arc069_b/24080584.cpp
@@ -0,0 +1,158 @@
+#pragma region template
+#include <atcoder/math>
+#include <atcoder/modint>
+#include <boost/range/irange.hpp>
+// #include <bits/stdc++.h>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <valarray>
+#include <vector>
+
+namespace mine {
+using atcoder::pow_mod, atcoder::inv_mod;
+using boost::irange;
+using std::cin, std::cout, std::endl;
+using std::min, std::max, std::sort, std::abs, std::pow;
+using std::vector, std::string, std::set, std::map;
+using mint = atcoder::modint1000000007;
+using ll = long long int;
+using Pa = std::pair<int, int>;
+using Vec = std::vector<int>;
+using VecVec = std::vector<Vec>;
+constexpr int MOD = 1000000007;
+constexpr int INF = std::numeric_limits<int>::max() / 2;
+constexpr ll LINF = std::numeric_limits<ll>::max() / 2;
+// clang-format off
+inline std::istream& operator>>(std::istream& is, mint& m) {ll n; is >> n; m = n; return is;}
+inline std::ostream& operator<<(std::ostream& os, const mint& m) { os << m.val(); return os;}
+template <class T> std::istream& operator>>(std::istream& is, vector<T>& vec) {for (auto&& v : vec) is >> v; return is;}
+template <class T> std::ostream& operator<<(std::ostream& os, const vector<T>& vec) {for (std::size_t i = 0; i < vec.size(); i++) {if (i) os << " "; os << vec[i];} return os;}
+template <class T> std::istream& operator>>(std::istream& is, std::valarray<T>& arr) {for (auto&& a : arr) is >> a; return is;}
+template <class T> std::ostream& operator<<(std::ostream& os, const std::valarray<T>& arr) {for (std::size_t i = 0; i < arr.size(); i++) {if (i) os << " "; os << arr[i];} return os;}
+template <class S, class T> std::istream& operator>>(std::istream& is, std::pair<S, T>& p) {is >> p.first >> p.second; return is;}
+template <class S, class T> std::ostream& operator<<(std::ostream& os, const std::pair<S, T>& p) {os << p.first << " " << p.second; return os;}
+template <class T> inline T input() {T n; cin >> n; return n;}
+template <class T> inline vector<T> input(int n) {vector<T> res(n); for (auto&& r : res) cin >> r; return res;}
+template <class T, size_t N> inline std::array<T, N> input() {std::array<T, N> res; for (auto&& r : res) cin >> r; return res;}
+template <class Tail> void print(Tail&& tail) {cout << tail << "\n";}
+template <class Head, class... Body> void print(Head&& head, Body&&... tail) {cout << head << " "; print(std::forward<Body>(tail)...);}
+template <class Tail> void debug(Tail&& tail) {std::clog << tail << endl;}
+template <class Head, class... Body> void debug(Head&& head, Body&&... tail) {std::clog << head << " "; debug(std::forward<Body>(tail)...);}
+template <class T> inline bool chmax(T& a, const T& b) {if (a < b) {a = b; return 1;} return 0;}
+template <class T> inline bool chmin(T& a, const T& b) {if (b < a) {a = b; return 1;} return 0;}
+inline void init() {cin.tie(nullptr); std::ios::sync_with_stdio(false); cout << std::fixed << std::setprecision(11) << std::boolalpha;}
+// clang-format on
+#pragma endregion
+
+string solve(bool zero, bool one, string& s, int n) {
+    string res;
+
+    if (zero) {
+        res.push_back('S');
+    } else {
+        res.push_back('W');
+    }
+    if (one) {
+        res.push_back('S');
+    } else {
+        res.push_back('W');
+    }
+
+    for (int i = 1; i < n - 1; i++) {
+        if (one) {
+            if (s[i] == 'o') {
+                bool temp = one;
+                one = zero;
+                zero = temp;
+            } else {
+                bool temp = one;
+                one = !zero;
+                zero = temp;
+            }
+        } else {
+            if (s[i] == 'x') {
+                bool temp = one;
+                one = zero;
+                zero = temp;
+            } else {
+                bool temp = one;
+                one = !zero;
+                zero = temp;
+            }
+        }
+
+        if (one) {
+            res.push_back('S');
+        } else {
+            res.push_back('W');
+        }
+    }
+
+    return res;
+}
+
+void main() {
+    auto n = input<int>();
+    auto s = input<string>();
+
+    vector<std::pair<bool, bool>> cand = {
+        {true, true}, {true, false}, {false, true}, {false, false}};
+
+    for (auto [o, t] : cand) {
+        auto res = solve(o, t, s, n);
+        bool flg = true;
+        for (int i = 0; i < n; i++) {
+            int b = i - 1, f = i + 1;
+            if (b == -1) b = n - 1;
+            if (f == n) f = 0;
+            if (res[i] == 'S') {
+                if (s[i] == 'o') {
+                    if (res[b] != res[f]) {
+                        flg = false;
+                        break;
+                    }
+                } else {
+                    if (res[b] == res[f]) {
+                        flg = false;
+                        break;
+                    }
+                }
+            } else {
+                if (s[i] == 'o') {
+                    if (res[b] == res[f]) {
+                        flg = false;
+                        break;
+                    }
+                } else {
+                    if (res[b] != res[f]) {
+                        flg = false;
+                        break;
+                    }
+                }
+            }
+        }
+        if (flg) {
+            print(res);
+            return;
+        }
+    }
+    print("-1");
+}
+
+}  // namespace mine
+
+int main() {
+    mine::init();
+    mine::main();
+    std::cout << std::flush;
+    return 0;
+}
