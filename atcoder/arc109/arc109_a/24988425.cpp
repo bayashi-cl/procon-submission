9bb477dc7d0914861ce1d82fe03c3affdc7a479d
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-08-13 13:41:49 +0900

    arc109_a

---
+++ atcoder/arc109/arc109_a/24988425.cpp
@@ -0,0 +1,148 @@
+#include <atcoder/math>
+#include <atcoder/modint>
+#include <boost/range/irange.hpp>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <valarray>
+#include <vector>
+
+namespace bys {
+using atcoder::pow_mod, atcoder::inv_mod;
+using boost::irange;
+using std::cin, std::cout, std::endl;
+using std::min, std::max, std::sort, std::abs, std::pow;
+using std::vector, std::string, std::set, std::map, std::pair;
+using mint = atcoder::modint1000000007;
+using ll = long long int;
+using Pa = std::pair<int, int>;
+using Vec = std::vector<int>;
+using VecVec = std::vector<Vec>;
+constexpr int MOD = 1000000007;
+constexpr int INF = std::numeric_limits<int>::max() / 2;
+constexpr ll LINF = std::numeric_limits<ll>::max() / 2;
+// clang-format off
+inline std::istream& operator>>(std::istream& is, mint& m) {ll n; is >> n; m = n; return is;}
+inline std::ostream& operator<<(std::ostream& os, const mint& m) { os << m.val(); return os;}
+template <class T> std::istream& operator>>(std::istream& is, vector<T>& vec) {for (auto&& v : vec) is >> v; return is;}
+template <class T> std::ostream& operator<<(std::ostream& os, const vector<T>& vec) {for (std::size_t i = 0; i < vec.size(); i++) {if (i) os << " "; os << vec[i];} return os;}
+template <class T> std::istream& operator>>(std::istream& is, std::valarray<T>& arr) {for (auto&& a : arr) is >> a; return is;}
+template <class T> std::ostream& operator<<(std::ostream& os, const std::valarray<T>& arr) {for (std::size_t i = 0; i < arr.size(); i++) {if (i) os << " "; os << arr[i];} return os;}
+template <class S, class T> std::istream& operator>>(std::istream& is, std::pair<S, T>& p) {is >> p.first >> p.second; return is;}
+template <class S, class T> std::ostream& operator<<(std::ostream& os, const std::pair<S, T>& p) {os << p.first << " " << p.second; return os;}
+template <class T> inline T input() {T n; cin >> n; return n;}
+template <class T> inline vector<T> input(int n) {vector<T> res(n); for (auto&& r : res) cin >> r; return res;}
+template <class T, size_t N> inline std::array<T, N> input() {std::array<T, N> res; for (auto&& r : res) cin >> r; return res;}
+template <class Tail> void print(Tail&& tail) {cout << tail << "\n";}
+template <class Head, class... Body> void print(Head&& head, Body&&... tail) {cout << head << " "; print(std::forward<Body>(tail)...);}
+template <class Tail> void debug(Tail&& tail) {std::clog << tail << endl;}
+template <class Head, class... Body> void debug(Head&& head, Body&&... tail) {std::clog << head << " "; debug(std::forward<Body>(tail)...);}
+template <class T> inline bool chmax(T& a, const T& b) {if (a < b) {a = b; return 1;} return 0;}
+template <class T> inline bool chmin(T& a, const T& b) {if (b < a) {a = b; return 1;} return 0;}
+void init() {cin.tie(nullptr); std::ios::sync_with_stdio(false); cout << std::fixed << std::setprecision(11) << std::boolalpha;}
+// clang-format on
+}  // namespace bys
+
+namespace bys {
+/**
+ * @brief ダイクストラ法による最短経路探索
+ * 構築後にDijkstra.search()を実行する
+ * O(|E| log|N|)
+ * @param graph 隣接グラフ
+ */
+struct Dijkstra {
+    using Edge = std::pair<ll, int>;  // (cost, node)
+    using Adj = vector<vector<Edge>>;
+    Adj graph;
+    vector<ll> cost;
+    vector<int> prev;
+    int n_node;
+
+    Dijkstra(const Adj& graph) : graph(graph) {
+        n_node = graph.size();
+        cost.resize(n_node, LINF);
+        prev.resize(n_node, -1);
+    }
+
+    /**
+     * @brief 探索
+     * コストはself.costに格納される。
+     * @param start 探索開始点
+     */
+    void search(int start) {
+        std::priority_queue<Edge, vector<Edge>, std::greater<Edge>> que;
+        cost[start] = 0;
+        que.push(Edge(0, start));
+
+        while (!que.empty()) {
+            auto [top_cost, top_node] = que.top();
+            que.pop();
+            if (cost[top_node] < top_cost) continue;
+            for (auto [adj_cost, adj_node] : graph[top_node]) {
+                ll temp_cost = cost[top_node] + adj_cost;
+                if (temp_cost < cost[adj_node]) {
+                    cost[adj_node] = temp_cost;
+                    prev[adj_node] = top_node;
+                    que.push(Edge(temp_cost, adj_node));
+                }
+            }
+        }
+    }
+
+    /**
+     * @brief 経路復元
+     * @param to
+     * @return vector<int>
+     */
+    vector<int> path_finder(int to) {
+        assert(to <= n_node);
+        vector<int> path;
+        while (to != -1) {
+            path.push_back(to);
+            to = prev[to];
+        }
+        std::reverse(std::begin(path), std::end(path));
+        return path;
+    }
+
+    ll get_cost(int i) {
+        ll res = cost[i];
+        if (res == LINF) res = -1;
+        return res;
+    }
+};
+void solve() {
+    auto [a, b, x, y] = input<int, 4>();
+    Dijkstra::Adj adj(201);
+    for (int i = 1; i <= 99; ++i) {
+        adj[i].push_back({x, i + 100});
+        adj[i + 100].push_back({x, i});
+
+        adj[i + 1].push_back({x, i + 100});
+        adj[i + 100].push_back({x, i + 1});
+
+        adj[i].push_back({y, i + 1});
+        adj[i + 1].push_back({y, i});
+    }
+    adj[100].push_back({x, 200});
+    adj[200].push_back({x, 100});
+    Dijkstra dij(adj);
+    dij.search(a);
+    print(dij.cost[b + 100]);
+}
+}  // namespace bys
+
+int main() {
+    bys::init();
+    bys::solve();
+    std::cout << std::flush;
+    return 0;
+}
