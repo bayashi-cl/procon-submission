6f2848f317e737b68dfb7e60054b851924bb5977
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-08-30 21:36:44 +0900

    tenka1_2019_a

---
+++ atcoder/tenka1-2019-beginner/tenka1_2019_a/25468675.py
@@ -0,0 +1,25 @@
+# region template
+import sys
+import typing
+from typing import Callable, Dict, List, Set, Tuple
+
+sys.setrecursionlimit(10 ** 6)
+Vec = List[int]
+VecVec = List[Vec]
+sinput: Callable[..., str] = sys.stdin.readline
+MOD: int = 1000000007
+INF: float = float("Inf")
+IINF: int = sys.maxsize
+# endregion
+
+
+def main() -> None:
+    a, b, c = map(int, sinput().split())
+    if a < c < b or b < c < a:
+        print("Yes")
+    else:
+        print("No")
+
+
+if __name__ == "__main__":
+    main()
