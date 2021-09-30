a5f71e0b2758447f53b3070c6e0781c1e0172861
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-09-30 22:37:47 +0900

    ALDS1_5_B

---
+++ AOJ/ALDS1_5_B/5926661.cpp
@@ -0,0 +1,92 @@
+#include <cassert>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <valarray>
+#include <vector>
+
+namespace bys {
+using std::cin, std::cout, std::endl;
+using std::min, std::max, std::sort, std::abs, std::pow;
+using std::vector, std::string, std::set, std::map, std::pair;
+using ll = long long int;
+using Pa = std::pair<int, int>;
+using Vec = std::vector<int>;
+using VecVec = std::vector<Vec>;
+constexpr int MOD = 1000000007;
+constexpr int INF = std::numeric_limits<int>::max() / 2;
+constexpr ll LINF = std::numeric_limits<ll>::max() / 2;
+// clang-format off
+template <class T> std::istream& operator>>(std::istream& is, vector<T>& vec) {for (auto&& v : vec) is >> v; return is;}
+template <class T> std::ostream& operator<<(std::ostream& os, const vector<T>& vec) {for (std::size_t i = 0; i < vec.size(); i++) {if (i) os << " "; os << vec[i];} return os;}
+template <class T> std::istream& operator>>(std::istream& is, std::valarray<T>& arr) {for (auto&& a : arr) is >> a; return is;}
+template <class T> std::ostream& operator<<(std::ostream& os, const std::valarray<T>& arr) {for (std::size_t i = 0; i < arr.size(); i++) {if (i) os << " "; os << arr[i];} return os;}
+template <class S, class T> std::istream& operator>>(std::istream& is, std::pair<S, T>& p) {is >> p.first >> p.second; return is;}
+template <class S, class T> std::ostream& operator<<(std::ostream& os, const std::pair<S, T>& p) {os << p.first << " " << p.second; return os;}
+template <class T> inline T input() {T n; cin >> n; return n;}
+template <class T> inline vector<T> input(int n) {vector<T> res(n); for (auto&& r : res) cin >> r; return res;}
+template <class T, size_t N> inline std::array<T, N> input() {std::array<T, N> res; for (auto&& r : res) cin >> r; return res;}
+template <class Tail> void print(Tail&& tail) {cout << tail << "\n";}
+template <class Head, class... Body> void print(Head&& head, Body&&... tail) {cout << head << " "; print(std::forward<Body>(tail)...);}
+template <class Tail> void debug(Tail&& tail) {std::clog << tail << endl;}
+template <class Head, class... Body> void debug(Head&& head, Body&&... tail) {std::clog << head << " "; debug(std::forward<Body>(tail)...);}
+template <class T> inline bool chmax(T& a, const T& b) {if (a < b) {a = b; return 1;} return 0;}
+template <class T> inline bool chmin(T& a, const T& b) {if (b < a) {a = b; return 1;} return 0;}
+void init() {cin.tie(nullptr); std::ios::sync_with_stdio(false); cout << std::fixed << std::setprecision(11) << std::boolalpha;}
+// clang-format on
+}  // namespace bys
+
+namespace bys {
+int CNT = 0;
+void merge(vector<int>& A, int left, int mid, int right) {
+    // [left, right) -> [left, mid), [mid, right)
+
+    Vec L(A.begin() + left, A.begin() + mid);
+    Vec R(A.begin() + mid, A.begin() + right);
+    L.push_back(INF);
+    R.push_back(INF);
+
+    int i = 0, j = 0;
+    for (int k = left; k < right; ++k) {
+        ++CNT;
+        if (L[i] <= R[j]) {
+            A[k] = L[i];
+            ++i;
+        } else {
+            A[k] = R[j];
+            ++j;
+        }
+    }
+}
+void merge_sort(vector<int>& A, int left, int right) {
+    if (left + 1 < right) {
+        int mid = (left + right) / 2;
+        merge_sort(A, left, mid);
+        merge_sort(A, mid, right);
+        merge(A, left, mid, right);
+    }
+}
+void solve() {
+    auto n = input<int>();
+    auto a = input<int>(n);
+    merge_sort(a, 0, n);
+    print(a);
+    print(CNT);
+}
+}  // namespace bys
+
+int main() {
+    bys::init();
+    bys::solve();
+    std::cout << std::flush;
+    return 0;
+}
+
