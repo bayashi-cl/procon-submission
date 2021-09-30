ecdc1f57cd9c6e84172b4a5468cabab328e3448c
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-09-30 19:15:19 +0900

    ALDS1_4_D

---
+++ AOJ/ALDS1_4_D/5925896.cpp
@@ -0,0 +1,91 @@
+#include <cassert>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <valarray>
+#include <vector>
+
+namespace bys {
+using std::cin, std::cout, std::endl;
+using std::min, std::max, std::sort, std::abs, std::pow;
+using std::vector, std::string, std::set, std::map, std::pair;
+using ll = long long int;
+using Pa = std::pair<int, int>;
+using Vec = std::vector<int>;
+using VecVec = std::vector<Vec>;
+constexpr int MOD = 1000000007;
+constexpr int INF = std::numeric_limits<int>::max() / 2;
+constexpr ll LINF = std::numeric_limits<ll>::max() / 2;
+// clang-format off
+template <class T> std::istream& operator>>(std::istream& is, vector<T>& vec) {for (auto&& v : vec) is >> v; return is;}
+template <class T> std::ostream& operator<<(std::ostream& os, const vector<T>& vec) {for (std::size_t i = 0; i < vec.size(); i++) {if (i) os << " "; os << vec[i];} return os;}
+template <class T> std::istream& operator>>(std::istream& is, std::valarray<T>& arr) {for (auto&& a : arr) is >> a; return is;}
+template <class T> std::ostream& operator<<(std::ostream& os, const std::valarray<T>& arr) {for (std::size_t i = 0; i < arr.size(); i++) {if (i) os << " "; os << arr[i];} return os;}
+template <class S, class T> std::istream& operator>>(std::istream& is, std::pair<S, T>& p) {is >> p.first >> p.second; return is;}
+template <class S, class T> std::ostream& operator<<(std::ostream& os, const std::pair<S, T>& p) {os << p.first << " " << p.second; return os;}
+template <class T> inline T input() {T n; cin >> n; return n;}
+template <class T> inline vector<T> input(int n) {vector<T> res(n); for (auto&& r : res) cin >> r; return res;}
+template <class T, size_t N> inline std::array<T, N> input() {std::array<T, N> res; for (auto&& r : res) cin >> r; return res;}
+template <class Tail> void print(Tail&& tail) {cout << tail << "\n";}
+template <class Head, class... Body> void print(Head&& head, Body&&... tail) {cout << head << " "; print(std::forward<Body>(tail)...);}
+template <class Tail> void debug(Tail&& tail) {std::clog << tail << endl;}
+template <class Head, class... Body> void debug(Head&& head, Body&&... tail) {std::clog << head << " "; debug(std::forward<Body>(tail)...);}
+template <class T> inline bool chmax(T& a, const T& b) {if (a < b) {a = b; return 1;} return 0;}
+template <class T> inline bool chmin(T& a, const T& b) {if (b < a) {a = b; return 1;} return 0;}
+void init() {cin.tie(nullptr); std::ios::sync_with_stdio(false); cout << std::fixed << std::setprecision(11) << std::boolalpha;}
+// clang-format on
+}  // namespace bys
+
+namespace bys {
+template <typename T, class Lambda, class... Args>
+T meguru_bisect(T ok, T ng, Lambda is_ok, Args... args) {
+    assert(is_ok(ok, args...));
+    assert(!is_ok(ng, args...));
+
+    while (std::abs(ok - ng) > 1) {
+        T mid = (ok + ng) / 2;
+        if (is_ok(mid, args...)) {
+            ok = mid;
+        } else {
+            ng = mid;
+        }
+    }
+    return ok;
+}
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    auto w = input<int>(n);
+    auto ok = [&](int x) {
+        int cnt = 1;
+        int cargo = 0;
+        for (auto&& wi : w) {
+            if (wi > x) return false;
+            if (cargo + wi > x) {
+                cnt++;
+                cargo = wi;
+            } else {
+                cargo += wi;
+            }
+        }
+        return cnt <= k;
+    };
+    print(meguru_bisect(MOD, 0, ok));
+}
+}  // namespace bys
+
+int main() {
+    bys::init();
+    bys::solve();
+    std::cout << std::flush;
+    return 0;
+}
+
