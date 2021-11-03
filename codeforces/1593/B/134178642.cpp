fb2cfb8fc32ff8cabd86119a238fcb8fd44862df
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-11-03 14:55:54 +0900

    B: Make it Divisible by 25

---
+++ codeforces/1593/B/134178642.cpp
@@ -0,0 +1,230 @@
+#line 2 "byslib/template/bys.hpp"
+#ifndef LOCAL
+#define NDEBUG
+#endif
+
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <complex>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+namespace bys {
+using std::array, std::vector, std::string, std::set, std::map, std::pair;
+using std::cin, std::cout, std::endl;
+using std::min, std::max, std::sort, std::reverse, std::abs, std::pow;
+
+// alias
+using ll = long long int;
+using ld = long double;
+using Pa = pair<int, int>;
+using Vec = vector<int>;
+using VecVec = std::vector<Vec>;
+template <class T>
+using uset = std::unordered_set<T>;
+template <class S, class T>
+using umap = std::unordered_map<S, T>;
+
+// const
+constexpr int MOD = 998244353;
+constexpr int INF = std::numeric_limits<int>::max() / 2;
+constexpr ll LINF = std::numeric_limits<ll>::max() / 2;
+
+// I/O
+// pair
+template <class T, class U>
+std::istream& operator>>(std::istream& is, std::pair<T, U>& p) {
+    return is >> p.first >> p.second;
+}
+template <typename T, typename U>
+std::ostream& operator<<(std::ostream& os, const std::pair<T, U>& p) {
+    return os << p.first << " " << p.second;
+}
+
+// STL container
+struct is_container_impl {
+    template <typename T>
+    static auto check(T&& obj)
+        -> decltype(obj.begin(), obj.end(), std::true_type{});
+    template <typename T>
+    static auto check(...) -> std::false_type;
+};
+template <typename T>
+class is_container
+    : public decltype(is_container_impl::check<T>(std::declval<T>())) {};
+
+template <typename C,
+          typename std::enable_if<is_container<C>::value &&
+                                      !std::is_same<C, std::string>::value &&
+                                      !std::is_same<C, std::wstring>::value,
+                                  std::nullptr_t>::type = nullptr>
+std::ostream& operator<<(std::ostream& os, const C& container) noexcept {
+    std::for_each(std::begin(container), std::prev(std::end(container)),
+                  [&](auto e) { os << e << ' '; });
+    return os << *std::prev(std::end(container));
+}
+
+template <typename C,
+          typename std::enable_if<is_container<C>::value &&
+                                      !std::is_same<C, std::string>::value &&
+                                      !std::is_same<C, std::wstring>::value,
+                                  std::nullptr_t>::type = nullptr>
+std::istream& operator>>(std::istream& is, C& container) {
+    std::for_each(std::begin(container), std::end(container),
+                  [&](auto&& e) { is >> e; });
+    return is;
+}
+
+// I/O helper
+template <class T>
+inline T input() {
+    T n;
+    cin >> n;
+    return n;
+}
+template <class T>
+inline vector<T> input(int n) {
+    vector<T> res(n);
+    cin >> res;
+    return res;
+}
+template <class T, size_t N>
+inline std::array<T, N> input() {
+    std::array<T, N> res;
+    cin >> res;
+    return res;
+}
+template <class S, class T, class... Us>
+std::tuple<S, T, Us...> input() {
+    std::tuple<S, T, Us...> res;
+    std::apply([](auto&... e) { (cin >> ... >> e); }, res);
+    return res;
+}
+
+struct Print {
+    std::ostream& os;
+    char sep = ' ', end = '\n';
+    Print() : os(std::cout) {}
+    Print(std::ostream& os) : os(os) {}
+    ~Print() { os << std::flush; }
+    void operator()() { os << end; }
+    template <class T>
+    void operator()(const T& a) {
+        os << a << end;
+    }
+    template <class T, class... Ts>
+    void operator()(const T& a, const Ts&... b) {
+        os << a;
+        (os << ... << (os << sep, b));
+        os << end;
+    }
+} print, debug(std::cerr);
+
+// utility
+template <class T>
+inline bool chmax(T& a, const T& b) {
+    if (a < b) {
+        a = b;
+        return 1;
+    }
+    return 0;
+}
+template <class T>
+inline bool chmin(T& a, const T& b) {
+    if (b < a) {
+        a = b;
+        return 1;
+    }
+    return 0;
+}
+template <class T>
+inline T iceil(T a, T b) {
+    return (a + b - 1) / b;
+}
+inline bool pop(int s, int d) { return s & (1 << d); }
+
+// config
+inline void init() {
+    cin.tie(nullptr);
+    std::ios::sync_with_stdio(false);
+    cout << std::fixed << std::setprecision(11);
+    std::cerr << std::boolalpha;
+}
+
+// macro
+#ifdef LOCAL
+#define DEBUG(...) debug("[debug] line:", __LINE__, "\n", __VA_ARGS__)
+#else
+#define DEBUG(...)
+#endif
+#define EXIT(...)           \
+    {                       \
+        print(__VA_ARGS__); \
+        return;             \
+    }
+}  // namespace bys
+#line 2 "B/main.cpp"
+
+namespace bys {
+void solve() {
+    auto n = input<string>();
+    reverse(std::begin(n), std::end(n));
+    int ans = INF;
+
+    if (auto itr_0 = std::find(std::begin(n), std::end(n), '0');
+        itr_0 != n.end()) {
+        int sel_0 = std::distance(n.begin(), itr_0);
+        // 00
+        auto itr_00 = std::find(itr_0 + 1, std::end(n), '0');
+        if (itr_00 != n.end()) {
+            int sel_00 = std::distance(itr_0 + 1, itr_00);
+            chmin(ans, sel_0 + sel_00);
+        }
+        // 50
+        auto itr_05 = std::find(itr_0 + 1, std::end(n), '5');
+        if (itr_05 != n.end()) {
+            int sel_05 = std::distance(itr_0 + 1, itr_05);
+            chmin(ans, sel_0 + sel_05);
+        }
+    }
+
+    if (auto itr_5 = std::find(std::begin(n), std::end(n), '5');
+        itr_5 != n.end()) {
+        int sel_5 = std::distance(n.begin(), itr_5);
+        // 25
+        auto itr_52 = std::find(itr_5 + 1, std::end(n), '2');
+        if (itr_52 != n.end()) {
+            int sel_52 = std::distance(itr_5 + 1, itr_52);
+            chmin(ans, sel_5 + sel_52);
+        }
+        // 75
+        auto itr_57 = std::find(itr_5 + 1, std::end(n), '7');
+        if (itr_57 != n.end()) {
+            int sel_57 = std::distance(itr_5 + 1, itr_57);
+            chmin(ans, sel_5 + sel_57);
+        }
+    }
+    print(ans);
+}
+}  // namespace bys
+
+int main() {
+    bys::init();
+    auto t = bys::input<int>();
+    for (int i = 0; i < t; ++i) bys::solve();
+    return 0;
+}
