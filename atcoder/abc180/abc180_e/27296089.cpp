f687a3f45ce1c91df424507483fad6b5b97d5013
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-11-17 04:02:07 +0900

    abc180_e

---
+++ atcoder/abc180/abc180_e/27296089.cpp
@@ -0,0 +1,225 @@
+#line 2 "byslib/template/bys.hpp"
+#ifndef LOCAL
+#define NDEBUG
+#endif
+
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <complex>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+namespace bys {
+using std::array, std::vector, std::string, std::set, std::map, std::pair;
+using std::cin, std::cout, std::endl;
+using std::min, std::max, std::sort, std::reverse, std::abs, std::pow;
+
+// alias
+using ll = long long int;
+using ld = long double;
+using Pa = pair<int, int>;
+using Vec = vector<int>;
+using VecVec = std::vector<Vec>;
+template <class T>
+using uset = std::unordered_set<T>;
+template <class S, class T>
+using umap = std::unordered_map<S, T>;
+
+// const
+constexpr int MOD = 998244353;
+constexpr int INF = std::numeric_limits<int>::max() / 2;
+constexpr ll LINF = std::numeric_limits<ll>::max() / 2;
+
+// I/O
+// pair
+template <class T, class U>
+std::istream& operator>>(std::istream& is, std::pair<T, U>& p) {
+    return is >> p.first >> p.second;
+}
+template <typename T, typename U>
+std::ostream& operator<<(std::ostream& os, const std::pair<T, U>& p) {
+    return os << p.first << " " << p.second;
+}
+
+// STL container
+struct is_container_impl {
+    template <typename T>
+    static auto check(T&& obj)
+        -> decltype(obj.begin(), obj.end(), std::true_type{});
+    template <typename T>
+    static auto check(...) -> std::false_type;
+};
+template <typename T>
+class is_container
+    : public decltype(is_container_impl::check<T>(std::declval<T>())) {};
+
+template <typename C,
+          typename std::enable_if<is_container<C>::value &&
+                                      !std::is_same<C, std::string>::value &&
+                                      !std::is_same<C, std::wstring>::value,
+                                  std::nullptr_t>::type = nullptr>
+std::ostream& operator<<(std::ostream& os, const C& container) noexcept {
+    std::for_each(std::begin(container), std::prev(std::end(container)),
+                  [&](auto e) { os << e << ' '; });
+    return os << *std::prev(std::end(container));
+}
+
+template <typename C,
+          typename std::enable_if<is_container<C>::value &&
+                                      !std::is_same<C, std::string>::value &&
+                                      !std::is_same<C, std::wstring>::value,
+                                  std::nullptr_t>::type = nullptr>
+std::istream& operator>>(std::istream& is, C& container) {
+    std::for_each(std::begin(container), std::end(container),
+                  [&](auto&& e) { is >> e; });
+    return is;
+}
+
+// I/O helper
+template <class T>
+inline T input() {
+    T n;
+    cin >> n;
+    return n;
+}
+template <class T>
+inline vector<T> input(int n) {
+    vector<T> res(n);
+    cin >> res;
+    return res;
+}
+template <class T, size_t N>
+inline std::array<T, N> input() {
+    std::array<T, N> res;
+    cin >> res;
+    return res;
+}
+template <class S, class T, class... Us>
+std::tuple<S, T, Us...> input() {
+    std::tuple<S, T, Us...> res;
+    std::apply([](auto&... e) { (cin >> ... >> e); }, res);
+    return res;
+}
+
+struct Print {
+    std::ostream& os;
+    char sep = ' ', end = '\n';
+    Print() : os(std::cout) {}
+    Print(std::ostream& os) : os(os) {}
+    ~Print() { os << std::flush; }
+    void operator()() { os << end; }
+    template <class T>
+    void operator()(const T& a) {
+        os << a << end;
+    }
+    template <class T, class... Ts>
+    void operator()(const T& a, const Ts&... b) {
+        os << a;
+        (os << ... << (os << sep, b));
+        os << end;
+    }
+} print, debug(std::cerr);
+
+// utility
+template <class T>
+inline bool chmax(T& a, const T& b) {
+    if (a < b) {
+        a = b;
+        return 1;
+    }
+    return 0;
+}
+template <class T>
+inline bool chmin(T& a, const T& b) {
+    if (b < a) {
+        a = b;
+        return 1;
+    }
+    return 0;
+}
+template <class T>
+inline T iceil(T a, T b) {
+    return (a + b - 1) / b;
+}
+inline bool pop(int s, int d) { return s & (1 << d); }
+
+// config
+inline void init() {
+    cin.tie(nullptr);
+    std::ios::sync_with_stdio(false);
+    cout << std::fixed << std::setprecision(11);
+    std::cerr << std::boolalpha;
+}
+
+// macro
+#ifdef LOCAL
+#define DEBUG(...) debug("[debug] line:", __LINE__, "\n", __VA_ARGS__)
+#else
+#define DEBUG(...)
+#endif
+// clang-format off
+#define EXIT(...) { print(__VA_ARGS__); return; }
+// clang-format on
+
+// solver
+void solve();
+void solver(int t = 1) {
+    for (int i = 0; i < t; ++i) solve();
+}
+}  // namespace bys
+#line 2 "abc180_e/main.cpp"
+
+namespace bys {
+struct City {
+    int x, y, z;
+    City(int x_, int y_, int z_) : x(x_), y(y_), z(z_) {}
+};
+int dist(City& a, City& b) {
+    return abs(b.x - a.x) + abs(b.y - a.y) + max(0, b.z - a.z);
+}
+void solve() {
+    auto n = input<int>();
+    vector<City> pos;
+    for (int i = 0; i < n; ++i) {
+        auto [x, y, z] = input<int, 3>();
+        pos.push_back({x, y, z});
+    }
+    vector dp(1 << n, vector(n, INF));
+    dp[1][0] = 0;
+    for (int s = 1; s < (1 << n); ++s) {
+        for (int d = 0; d < n; ++d) {
+            if (dp[s][d] == INF) continue;
+            for (int to = 0; to < n; ++to) {
+                if (pop(s, to)) continue;
+                int t = s | (1 << to);
+                chmin(dp[t][to], dp[s][d] + dist(pos[d], pos[to]));
+            }
+        }
+    }
+    int ans = INF;
+    for (int i = 0; i < n; ++i) {
+        chmin(ans, dp.back()[i] + dist(pos[i], pos[0]));
+    }
+    print(ans);
+}
+}  // namespace bys
+
+int main() {
+    bys::init();
+    bys::solver(/* bys::input<int>() */);
+    return 0;
+}
