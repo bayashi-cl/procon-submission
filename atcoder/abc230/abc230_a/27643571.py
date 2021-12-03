4ca86c49607cfc9f4ee3e12702c72aa25e9ee6b4
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-12-03 21:01:38 +0900

    abc230_a

---
+++ atcoder/abc230/abc230_a/27643571.py
@@ -0,0 +1,24 @@
+# region template
+import sys
+import typing
+from typing import Callable, Dict, List, Set, Tuple
+
+sys.setrecursionlimit(10 ** 6)
+Vec = List[int]
+VecVec = List[Vec]
+sinput: Callable[..., str] = sys.stdin.readline
+MOD: int = 998244353
+INF: float = float("Inf")
+IINF: int = sys.maxsize // 2
+# endregion
+
+
+def main() -> None:
+    n = int(sinput())
+    if n >= 42:
+        n += 1
+    print("AGC" + str(n).zfill(3))
+
+
+if __name__ == "__main__":
+    main()
