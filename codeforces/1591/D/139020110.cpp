c224061fba2d36665617d41b0853f535384a33f4
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-12-14 04:27:15 +0900

    D: Yet Another Sorting Problem

---
+++ codeforces/1591/D/139020110.cpp
@@ -0,0 +1,407 @@
+#line 1 "/home/bayashi/git/ac-library/atcoder/fenwicktree.hpp"
+
+
+
+#include <cassert>
+#include <vector>
+
+#line 1 "/home/bayashi/git/ac-library/atcoder/internal_type_traits.hpp"
+
+
+
+#line 5 "/home/bayashi/git/ac-library/atcoder/internal_type_traits.hpp"
+#include <numeric>
+#include <type_traits>
+
+namespace atcoder {
+
+namespace internal {
+
+#ifndef _MSC_VER
+template <class T>
+using is_signed_int128 =
+    typename std::conditional<std::is_same<T, __int128_t>::value ||
+                                  std::is_same<T, __int128>::value,
+                              std::true_type,
+                              std::false_type>::type;
+
+template <class T>
+using is_unsigned_int128 =
+    typename std::conditional<std::is_same<T, __uint128_t>::value ||
+                                  std::is_same<T, unsigned __int128>::value,
+                              std::true_type,
+                              std::false_type>::type;
+
+template <class T>
+using make_unsigned_int128 =
+    typename std::conditional<std::is_same<T, __int128_t>::value,
+                              __uint128_t,
+                              unsigned __int128>;
+
+template <class T>
+using is_integral = typename std::conditional<std::is_integral<T>::value ||
+                                                  is_signed_int128<T>::value ||
+                                                  is_unsigned_int128<T>::value,
+                                              std::true_type,
+                                              std::false_type>::type;
+
+template <class T>
+using is_signed_int = typename std::conditional<(is_integral<T>::value &&
+                                                 std::is_signed<T>::value) ||
+                                                    is_signed_int128<T>::value,
+                                                std::true_type,
+                                                std::false_type>::type;
+
+template <class T>
+using is_unsigned_int =
+    typename std::conditional<(is_integral<T>::value &&
+                               std::is_unsigned<T>::value) ||
+                                  is_unsigned_int128<T>::value,
+                              std::true_type,
+                              std::false_type>::type;
+
+template <class T>
+using to_unsigned = typename std::conditional<
+    is_signed_int128<T>::value,
+    make_unsigned_int128<T>,
+    typename std::conditional<std::is_signed<T>::value,
+                              std::make_unsigned<T>,
+                              std::common_type<T>>::type>::type;
+
+#else
+
+template <class T> using is_integral = typename std::is_integral<T>;
+
+template <class T>
+using is_signed_int =
+    typename std::conditional<is_integral<T>::value && std::is_signed<T>::value,
+                              std::true_type,
+                              std::false_type>::type;
+
+template <class T>
+using is_unsigned_int =
+    typename std::conditional<is_integral<T>::value &&
+                                  std::is_unsigned<T>::value,
+                              std::true_type,
+                              std::false_type>::type;
+
+template <class T>
+using to_unsigned = typename std::conditional<is_signed_int<T>::value,
+                                              std::make_unsigned<T>,
+                                              std::common_type<T>>::type;
+
+#endif
+
+template <class T>
+using is_signed_int_t = std::enable_if_t<is_signed_int<T>::value>;
+
+template <class T>
+using is_unsigned_int_t = std::enable_if_t<is_unsigned_int<T>::value>;
+
+template <class T> using to_unsigned_t = typename to_unsigned<T>::type;
+
+}  // namespace internal
+
+}  // namespace atcoder
+
+
+#line 8 "/home/bayashi/git/ac-library/atcoder/fenwicktree.hpp"
+
+namespace atcoder {
+
+// Reference: https://en.wikipedia.org/wiki/Fenwick_tree
+template <class T> struct fenwick_tree {
+    using U = internal::to_unsigned_t<T>;
+
+  public:
+    fenwick_tree() : _n(0) {}
+    explicit fenwick_tree(int n) : _n(n), data(n) {}
+
+    void add(int p, T x) {
+        assert(0 <= p && p < _n);
+        p++;
+        while (p <= _n) {
+            data[p - 1] += U(x);
+            p += p & -p;
+        }
+    }
+
+    T sum(int l, int r) {
+        assert(0 <= l && l <= r && r <= _n);
+        return sum(r) - sum(l);
+    }
+
+  private:
+    int _n;
+    std::vector<U> data;
+
+    U sum(int r) {
+        U s = 0;
+        while (r > 0) {
+            s += data[r - 1];
+            r -= r & -r;
+        }
+        return s;
+    }
+};
+
+}  // namespace atcoder
+
+
+#line 2 "/home/bayashi/dev/byslib/byslib/template/bys.hpp"
+#ifndef LOCAL
+#define NDEBUG
+#endif
+
+#include <algorithm>
+#include <array>
+#line 9 "/home/bayashi/dev/byslib/byslib/template/bys.hpp"
+#include <cmath>
+#include <complex>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <map>
+#line 17 "/home/bayashi/dev/byslib/byslib/template/bys.hpp"
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#line 22 "/home/bayashi/dev/byslib/byslib/template/bys.hpp"
+#include <unordered_map>
+#include <unordered_set>
+#line 25 "/home/bayashi/dev/byslib/byslib/template/bys.hpp"
+
+namespace bys {
+using std::array, std::vector, std::string, std::set, std::map, std::pair;
+using std::cin, std::cout, std::endl;
+using std::min, std::max, std::sort, std::reverse, std::abs, std::pow;
+
+// alias
+using ll = long long int;
+using ld = long double;
+using Pa = pair<int, int>;
+using Vec = vector<int>;
+using VecVec = std::vector<Vec>;
+template <class T>
+using uset = std::unordered_set<T>;
+template <class S, class T>
+using umap = std::unordered_map<S, T>;
+
+// const
+constexpr int MOD = 998244353;
+constexpr int INF = std::numeric_limits<int>::max() / 2;
+constexpr ll LINF = std::numeric_limits<ll>::max() / 2;
+
+// I/O
+// pair
+template <class T, class U>
+std::istream& operator>>(std::istream& is, std::pair<T, U>& p) {
+    return is >> p.first >> p.second;
+}
+template <typename T, typename U>
+std::ostream& operator<<(std::ostream& os, const std::pair<T, U>& p) {
+    return os << p.first << " " << p.second;
+}
+
+// STL container
+struct is_container_impl {
+    template <typename T>
+    static auto check(T&& obj)
+        -> decltype(obj.begin(), obj.end(), std::true_type{});
+    template <typename T>
+    static auto check(...) -> std::false_type;
+};
+template <typename T>
+class is_container
+    : public decltype(is_container_impl::check<T>(std::declval<T>())) {};
+
+template <typename C,
+          typename std::enable_if<is_container<C>::value &&
+                                      !std::is_same<C, std::string>::value &&
+                                      !std::is_same<C, std::wstring>::value,
+                                  std::nullptr_t>::type = nullptr>
+std::ostream& operator<<(std::ostream& os, const C& container) noexcept {
+    if (container.empty()) return os;
+    std::for_each(std::begin(container), std::prev(std::end(container)),
+                  [&](auto e) { os << e << ' '; });
+    return os << *std::prev(std::end(container));
+}
+
+template <typename C,
+          typename std::enable_if<is_container<C>::value &&
+                                      !std::is_same<C, std::string>::value &&
+                                      !std::is_same<C, std::wstring>::value,
+                                  std::nullptr_t>::type = nullptr>
+std::istream& operator>>(std::istream& is, C& container) {
+    std::for_each(std::begin(container), std::end(container),
+                  [&](auto&& e) { is >> e; });
+    return is;
+}
+
+// I/O helper
+template <class T>
+inline T input() {
+    T n;
+    cin >> n;
+    return n;
+}
+template <class T>
+inline vector<T> input(int n) {
+    vector<T> res(n);
+    cin >> res;
+    return res;
+}
+template <class T>
+inline vector<vector<T>> input(int n, int m) {
+    vector res(n, vector<T>(m));
+    cin >> res;
+    return res;
+}
+template <class T, size_t N>
+inline std::array<T, N> input() {
+    std::array<T, N> res;
+    cin >> res;
+    return res;
+}
+template <class S, class T, class... Us>
+std::tuple<S, T, Us...> input() {
+    std::tuple<S, T, Us...> res;
+    std::apply([](auto&... e) { (cin >> ... >> e); }, res);
+    return res;
+}
+
+struct Print {
+    std::ostream& os;
+    string sep = " ", end = "\n";
+    Print(std::ostream& os) : os(os) {}
+    ~Print() { os << std::flush; }
+    void operator()() { os << end; }
+    template <class T>
+    void operator()(const T& a) {
+        os << a << end;
+    }
+    template <class T, class... Ts>
+    void operator()(const T& a, const Ts&... b) {
+        os << a;
+        (os << ... << (os << sep, b));
+        os << end;
+    }
+    template <class... Ts>
+    void send(const Ts&... a) {
+        operator()(a...);
+        os << std::flush;
+    }
+} print(std::cout), debug(std::cerr);
+
+// utility
+template <class T>
+inline bool chmax(T& a, const T& b) {
+    if (a < b) {
+        a = b;
+        return 1;
+    }
+    return 0;
+}
+template <class T>
+inline bool chmin(T& a, const T& b) {
+    if (b < a) {
+        a = b;
+        return 1;
+    }
+    return 0;
+}
+template <class T>
+inline T iceil(T a, T b) {
+    return (a + b - 1) / b;
+}
+inline bool pop(int s, int d) { return s & (1 << d); }
+inline bool pop(ll s, int d) { return s & (1LL << d); }
+template <typename T>
+struct Range {
+    Range(T start, T stop, T step = 1)
+        : it(start), stop(stop), step(step), dir(step >= 0 ? 1 : -1) {}
+    Range(T stop) : it(0), stop(stop), step(1), dir(1) {}
+    Range<T> begin() const { return *this; }
+    T end() const { return stop; }
+    bool operator!=(const T val) const { return (val - it) * dir > 0; }
+    void operator++() { it += step; }
+    const T& operator*() const { return it; }
+
+   private:
+    T it;
+    const T stop, step;
+    const int dir;
+
+    friend Range reversed(const Range& r) {
+        auto new_start = (r.stop - r.dir - r.it) / r.step * r.step + r.it;
+        return {new_start, r.it - r.dir, -r.step};
+    }
+};
+
+// config
+inline void init() {
+    cin.tie(nullptr);
+    std::ios::sync_with_stdio(false);
+    cout << std::fixed << std::setprecision(11);
+    std::cerr << std::boolalpha;
+}
+
+// macro
+#ifdef LOCAL
+#define DEBUG(...) debug("[debug] line:", __LINE__, "\n", __VA_ARGS__)
+#else
+#define DEBUG(...)
+#endif
+// clang-format off
+#define EXIT(...) { print(__VA_ARGS__); return; }
+// clang-format on
+
+// solver
+struct Solver {
+    Solver() { init(); }
+    void solve();
+    void solve(int rep) {
+        for (int i = 0; i < rep; ++i) solve();
+    }
+};
+}  // namespace bys
+#line 3 "D/main.cpp"
+
+namespace bys {
+// template <typename T>
+// struct BinaryIndexedTree {
+//     vector<T> data;
+//     BinaryIndexedTree(int sz) { data.assign(++sz, 0); }
+
+//     T sum(int k) {
+//         T ret = 0;
+//         for (++k; k > 0; k -= k & -k) ret += data[k];
+//         return (ret);
+//     }
+
+//     void add(int k, T x) {
+//         for (++k; k < (int)data.size(); k += k & -k) data[k] += x;
+//     }
+// };
+void Solver::solve() {
+    auto n = input<int>();
+    auto a = input<int>(n);
+    set<int> s(a.begin(), a.end());
+    if ((int)s.size() != n) EXIT("YES");
+    atcoder::fenwick_tree<int> bit(n + 1);
+    ll cnt = 0;
+    for (int ai : a) {
+        cnt += bit.sum(ai + 1, n + 1);
+        bit.add(ai, 1);
+    }
+    print(cnt % 2 == 0 ? "YES" : "NO");
+}
+}  // namespace bys
+
+int main() {
+    bys::Solver solver;
+    solver.solve(bys::input<int>());
+    return 0;
+}
