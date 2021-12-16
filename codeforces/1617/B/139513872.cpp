629831861c67dfe942ae7d1a807a7b5407402c04
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-12-16 23:25:10 +0900

    B: GCD Problem

---
+++ codeforces/1617/B/139513872.cpp
@@ -0,0 +1,378 @@
+#line 2 "/home/bayashi/dev/byslib/byslib/template/bys.hpp"
+#ifndef LOCAL
+#define NDEBUG
+#endif
+
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <complex>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+namespace bys {
+using std::array, std::vector, std::string, std::set, std::map, std::pair;
+using std::cin, std::cout, std::endl;
+using std::min, std::max, std::sort, std::reverse, std::abs, std::pow;
+
+// alias
+using ll = long long int;
+using ld = long double;
+using Pa = pair<int, int>;
+using Vec = vector<int>;
+using VecVec = std::vector<Vec>;
+template <class T>
+using uset = std::unordered_set<T>;
+template <class S, class T>
+using umap = std::unordered_map<S, T>;
+
+// const
+constexpr int MOD = 998244353;
+constexpr int INF = std::numeric_limits<int>::max() / 2;
+constexpr ll LINF = std::numeric_limits<ll>::max() / 2;
+
+// I/O
+// pair
+template <class T, class U>
+std::istream& operator>>(std::istream& is, std::pair<T, U>& p) {
+    return is >> p.first >> p.second;
+}
+template <typename T, typename U>
+std::ostream& operator<<(std::ostream& os, const std::pair<T, U>& p) {
+    return os << p.first << " " << p.second;
+}
+
+// STL container
+struct is_container_impl {
+    template <typename T>
+    static auto check(T&& obj)
+        -> decltype(obj.begin(), obj.end(), std::true_type{});
+    template <typename T>
+    static auto check(...) -> std::false_type;
+};
+template <typename T>
+class is_container
+    : public decltype(is_container_impl::check<T>(std::declval<T>())) {};
+
+template <typename C,
+          typename std::enable_if<is_container<C>::value &&
+                                      !std::is_same<C, std::string>::value &&
+                                      !std::is_same<C, std::wstring>::value,
+                                  std::nullptr_t>::type = nullptr>
+std::ostream& operator<<(std::ostream& os, const C& container) noexcept {
+    if (container.empty()) return os;
+    std::for_each(std::begin(container), std::prev(std::end(container)),
+                  [&](auto e) { os << e << ' '; });
+    return os << *std::prev(std::end(container));
+}
+
+template <typename C,
+          typename std::enable_if<is_container<C>::value &&
+                                      !std::is_same<C, std::string>::value &&
+                                      !std::is_same<C, std::wstring>::value,
+                                  std::nullptr_t>::type = nullptr>
+std::istream& operator>>(std::istream& is, C& container) {
+    std::for_each(std::begin(container), std::end(container),
+                  [&](auto&& e) { is >> e; });
+    return is;
+}
+
+// I/O helper
+template <class T>
+inline T input() {
+    T n;
+    cin >> n;
+    return n;
+}
+template <class T>
+inline vector<T> input(int n) {
+    vector<T> res(n);
+    cin >> res;
+    return res;
+}
+template <class T>
+inline vector<vector<T>> input(int n, int m) {
+    vector res(n, vector<T>(m));
+    cin >> res;
+    return res;
+}
+template <class T, size_t N>
+inline std::array<T, N> input() {
+    std::array<T, N> res;
+    cin >> res;
+    return res;
+}
+template <class S, class T, class... Us>
+std::tuple<S, T, Us...> input() {
+    std::tuple<S, T, Us...> res;
+    std::apply([](auto&... e) { (cin >> ... >> e); }, res);
+    return res;
+}
+
+struct Print {
+    std::ostream& os;
+    string sep = " ", end = "\n";
+    Print(std::ostream& os) : os(os) {}
+    ~Print() { os << std::flush; }
+    void operator()() { os << end; }
+    template <class T>
+    void operator()(const T& a) {
+        os << a << end;
+    }
+    template <class T, class... Ts>
+    void operator()(const T& a, const Ts&... b) {
+        os << a;
+        (os << ... << (os << sep, b));
+        os << end;
+    }
+    template <class... Ts>
+    void send(const Ts&... a) {
+        operator()(a...);
+        os << std::flush;
+    }
+} print(std::cout), debug(std::cerr);
+
+// utility
+template <class T>
+inline bool chmax(T& a, const T& b) {
+    if (a < b) {
+        a = b;
+        return 1;
+    }
+    return 0;
+}
+template <class T>
+inline bool chmin(T& a, const T& b) {
+    if (b < a) {
+        a = b;
+        return 1;
+    }
+    return 0;
+}
+template <class T>
+inline T iceil(T a, T b) {
+    return (a + b - 1) / b;
+}
+inline bool pop(int s, int d) { return s & (1 << d); }
+inline bool pop(ll s, int d) { return s & (1LL << d); }
+template <typename T>
+struct Range {
+    Range(T start, T stop, T step = 1)
+        : it(start), stop(stop), step(step), dir(step >= 0 ? 1 : -1) {}
+    Range(T stop) : it(0), stop(stop), step(1), dir(1) {}
+    Range<T> begin() const { return *this; }
+    T end() const { return stop; }
+    bool operator!=(const T val) const { return (val - it) * dir > 0; }
+    void operator++() { it += step; }
+    const T& operator*() const { return it; }
+
+   private:
+    T it;
+    const T stop, step;
+    const int dir;
+
+    friend Range reversed(const Range& r) {
+        auto new_start = (r.stop - r.dir - r.it) / r.step * r.step + r.it;
+        return {new_start, r.it - r.dir, -r.step};
+    }
+};
+
+// config
+inline void init() {
+    cin.tie(nullptr);
+    std::ios::sync_with_stdio(false);
+    cout << std::fixed << std::setprecision(11);
+    std::cerr << std::boolalpha;
+}
+
+// macro
+#ifdef LOCAL
+#define DEBUG(...) debug("[debug] line:", __LINE__, "\n", __VA_ARGS__)
+#else
+#define DEBUG(...)
+#endif
+// clang-format off
+#define EXIT(...) { print(__VA_ARGS__); return; }
+// clang-format on
+
+// solver
+struct Solver {
+    Solver() { init(); }
+    void solve();
+    void solve(int rep) {
+        for (int i = 0; i < rep; ++i) solve();
+    }
+};
+}  // namespace bys
+#line 3 "/home/bayashi/dev/byslib/byslib/integer.hpp"
+
+namespace bys {
+constexpr ll int_pow(int a, int b) {
+    ll res = 1;
+    for (int i = 0; i < b; ++i) res *= a;
+    return res;
+}
+
+template <typename T>
+vector<T> make_divisor(T n) {
+    vector<T> lower, upper;
+    for (int i = 1; i * i <= n; ++i) {
+        if (n % i == 0) {
+            lower.push_back(i);
+            if (T j = n / i; i != j) upper.push_back(j);
+        }
+    }
+    std::copy(upper.rbegin(), upper.rend(), std::back_inserter(lower));
+    return lower;
+}
+
+vector<vector<int>> eratosthenes_factorize(int n) {
+    vector<vector<int>> res(n + 1);
+    for (int i = 2; i <= n; i += 2) res[i].push_back(2);
+    for (int p = 3; p <= n; p += 2) {
+        if (!res[p].empty()) continue;
+        for (int k = 1; p * k <= n; ++k) {
+            res[p * k].push_back(p);
+        }
+    }
+    return res;
+}
+struct Osa_K {
+    int mx;
+    vector<int> spf;  // smallest prime factor
+    Osa_K(int n) : mx(n), spf(n + 1) {
+        std::iota(spf.begin(), spf.end(), 0);
+        sieve();
+    }
+
+    void sieve() {
+        for (int i = 2; i <= mx; i += 2) spf[i] = 2;
+        for (int p = 3; p * p <= mx; p += 2) {
+            if (spf[p] != p) continue;
+            for (int k = p * p; k <= mx; k += p) {
+                if (spf[k] == k) spf[k] = p;
+            }
+        }
+    }
+    bool is_prime(int n) const {
+        assert(n <= mx);
+        if (n <= 1) return false;
+        return spf[n] == n;
+    }
+    map<int, int> factorize(int n) const {
+        assert(n <= mx);
+        map<int, int> res;
+        while (n > 1) {
+            res[spf[n]]++;
+            n /= spf[n];
+        }
+        return res;
+    }
+    vector<int> divisor(int n) const {
+        vector<int> res;
+        map<int, int> factor_ = factorize(n);
+        vector<pair<int, int>> factor(factor_.begin(), factor_.end());
+        int k = factor.size();
+        auto dfs = [&](auto dfs, int depth, int prod) {
+            if (depth == k) {
+                res.push_back(prod);
+                return;
+            }
+            auto [p, q] = factor[depth];
+            for (int i = 0; i <= q; ++i) {
+                dfs(dfs, depth + 1, prod);
+                prod *= p;
+            }
+        };
+        dfs(dfs, 0, 1);
+        sort(res.begin(), res.end());
+        return res;
+    }
+};
+
+struct MultiComb {
+    int _n;
+    int mod;
+    vector<ll> fact, factinv, inv;
+
+    MultiComb(int n, int mod = MOD) : _n(n), mod(mod) {
+        fact.resize(_n + 1);
+        factinv.resize(_n + 1);
+        inv.resize(_n + 1);
+
+        fact[0] = fact[1] = 1;
+        factinv[0] = factinv[1] = 1;
+        inv[1] = 1;
+
+        for (int i = 2; i <= _n; i++) {
+            fact[i] = fact[i - 1] * i % mod;
+            inv[i] = mod - inv[mod % i] * (mod / i) % mod;
+            factinv[i] = factinv[i - 1] * inv[i] % mod;
+        }
+    }
+
+    ll comb(int n, int r) {
+        if (r < 0 || n < r) return 0;
+        return fact[n] * (factinv[r] * factinv[n - r] % mod) % mod;
+    }
+};
+
+template <typename T>
+vector<T> prime_factorize(T n) {
+    vector<T> res;
+    while (n % 2 == 0) {
+        res.push_back(2);
+        n /= 2;
+    }
+    T f = 3;
+    while (f * f <= n) {
+        if (n % f == 0) {
+            res.push_back(f);
+            n /= f;
+        } else {
+            f += 2;
+        }
+    }
+    if (n != 1) res.push_back(n);
+    return res;
+}
+}  // namespace bys
+#line 3 "B/main.cpp"
+
+namespace bys {
+bool check(int a, int b, int c, int n) {
+    return a + b + c == n && std::gcd(a, b) == c;
+}
+void Solver::solve() {
+    auto n = input<int>();
+    Osa_K osa_k(1000);
+    int c = 1;
+    for (int i = 2; i < 1000; ++i) {
+        if (!osa_k.is_prime(i)) continue;
+        if ((n - 1) % i == 0) continue;
+        int a = i;
+        int b = n - 1 - i;
+        DEBUG(check(a, b, c, n));
+        EXIT(a, b, c);
+    }
+}
+}  // namespace bys
+
+int main() {
+    bys::Solver solver;
+    solver.solve(bys::input<int>());
+    return 0;
+}
