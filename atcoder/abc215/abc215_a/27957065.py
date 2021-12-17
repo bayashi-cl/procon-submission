489859627245d5ed96b39abe59669993dbbefc35
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-12-18 01:41:46 +0900

    abc215_a

---
+++ atcoder/abc215/abc215_a/27957065.py
@@ -0,0 +1,25 @@
+# region template
+import sys
+import typing
+from typing import Callable, Dict, List, Set, Tuple
+
+sys.setrecursionlimit(10 ** 6)
+Vec = List[int]
+VecVec = List[Vec]
+sinput: Callable[..., str] = sys.stdin.readline
+MOD: int = 998244353
+INF: float = float("Inf")
+IINF: int = sys.maxsize // 2
+# endregion
+
+
+def main() -> None:
+    s = sinput().strip()
+    if s == "Hello,World!":
+        print("AC")
+    else:
+        print("WA")
+
+
+if __name__ == "__main__":
+    main()
