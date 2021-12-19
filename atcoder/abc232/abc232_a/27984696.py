05d6eefd1daa1b4a8f72c3b108bbc7cb050a8be4
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-12-19 21:01:10 +0900

    abc232_a

---
+++ atcoder/abc232/abc232_a/27984696.py
@@ -0,0 +1,22 @@
+# region template
+import sys
+import typing
+from typing import Callable, Dict, List, Set, Tuple
+
+sys.setrecursionlimit(10 ** 6)
+Vec = List[int]
+VecVec = List[Vec]
+sinput: Callable[..., str] = sys.stdin.readline
+MOD: int = 998244353
+INF: float = float("Inf")
+IINF: int = sys.maxsize // 2
+# endregion
+
+
+def main() -> None:
+    a, b = map(int, sinput().split("x"))
+    print(a * b)
+
+
+if __name__ == "__main__":
+    main()
