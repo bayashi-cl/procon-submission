6b704567a9d244ee2382d80b7f77df81d0826e37
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-12-31 23:39:30 +0900

    abc197_d

---
+++ atcoder/abc197/abc197_d/28266213.cpp
@@ -0,0 +1,334 @@
+#line 2 "/home/bayashi/dev/byslib/core/stdlib.hpp"
+#ifndef LOCAL
+#define NDEBUG
+#endif
+
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <complex>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+namespace bys {
+using std::array, std::vector, std::string, std::set, std::map, std::pair;
+using std::cin, std::cout, std::endl;
+using std::min, std::max, std::sort, std::reverse, std::abs, std::pow;
+
+// alias
+using ll = long long int;
+using ld = long double;
+using Pa = pair<int, int>;
+using Pall = pair<ll, ll>;
+template <class T>
+using uset = std::unordered_set<T>;
+template <class S, class T>
+using umap = std::unordered_map<S, T>;
+}  // namespace bys
+#line 3 "/home/bayashi/dev/byslib/core/const.hpp"
+
+namespace bys {
+constexpr int MOD = 998244353;
+constexpr int MOD7 = 1000000007;
+constexpr int INF = std::numeric_limits<int>::max() / 2;
+constexpr ll LINF = std::numeric_limits<ll>::max() / 2;
+}  // namespace bys
+#line 3 "/home/bayashi/dev/byslib/core/io.hpp"
+
+namespace bys {
+// pair
+template <class T, class U>
+std::istream& operator>>(std::istream& is, std::pair<T, U>& p) {
+    return is >> p.first >> p.second;
+}
+template <typename T, typename U>
+std::ostream& operator<<(std::ostream& os, const std::pair<T, U>& p) {
+    return os << p.first << " " << p.second;
+}
+
+// STL container
+struct is_container_impl {
+    template <typename T>
+    static auto check(T&& obj) -> decltype(obj.begin(), obj.end(), std::true_type{});
+    template <typename T>
+    static auto check(...) -> std::false_type;
+};
+template <typename T>
+class is_container : public decltype(is_container_impl::check<T>(std::declval<T>())) {};
+
+template <typename C, typename std::enable_if<is_container<C>::value && !std::is_same<C, std::string>::value &&
+                                                  !std::is_same<C, std::wstring>::value,
+                                              std::nullptr_t>::type = nullptr>
+std::ostream& operator<<(std::ostream& os, const C& container) noexcept {
+    if (container.empty()) return os;
+    std::for_each(std::begin(container), std::prev(std::end(container)), [&](auto e) { os << e << ' '; });
+    return os << *std::prev(std::end(container));
+}
+
+template <typename C, typename std::enable_if<is_container<C>::value && !std::is_same<C, std::string>::value &&
+                                                  !std::is_same<C, std::wstring>::value,
+                                              std::nullptr_t>::type = nullptr>
+std::istream& operator>>(std::istream& is, C& container) {
+    std::for_each(std::begin(container), std::end(container), [&](auto&& e) { is >> e; });
+    return is;
+}
+
+// I/O helper
+//! @brief 任意の型を1つ
+template <class T>
+inline T input() {
+    T n;
+    cin >> n;
+    return n;
+}
+//! @brief 任意の型がn要素のvector
+template <class T>
+inline vector<T> input(int n) {
+    vector<T> res(n);
+    cin >> res;
+    return res;
+}
+//! @brief 任意の型がn行m列のvector
+template <class T>
+inline vector<vector<T>> input(int n, int m) {
+    vector res(n, vector<T>(m));
+    cin >> res;
+    return res;
+}
+
+//! @brief 任意の型をN個 受け取りは構造化束縛で
+template <class T, size_t N>
+inline std::array<T, N> input() {
+    std::array<T, N> res;
+    cin >> res;
+    return res;
+}
+//! @brief 2つ以上の異なる型 受け取りは構造化束縛で
+template <class S, class T, class... Us>
+std::tuple<S, T, Us...> input() {
+    std::tuple<S, T, Us...> res;
+    std::apply([](auto&... e) { (cin >> ... >> e); }, res);
+    return res;
+}
+//! @brief 標準入力から代入
+template <class... Ts>
+void cinto(Ts&... args) {
+    (cin >> ... >> args);
+}
+//! @brief pythonのprintっぽい挙動をする
+struct Print {
+    std::ostream& os;
+    string sep = " ", end = "\n";
+    Print(std::ostream& os) : os(os) {}
+    ~Print() { os << std::flush; }
+    void operator()() { os << end; }
+    template <class T>
+    void operator()(const T& a) {
+        os << a << end;
+    }
+    //! @brief 空白区切りで出力
+    template <class T, class... Ts>
+    void operator()(const T& a, const Ts&... b) {
+        os << a;
+        (os << ... << (os << sep, b));
+        os << end;
+    }
+    //! @brief 出力後flush インタラクティブ問題用
+    template <class... Ts>
+    void send(const Ts&... a) {
+        operator()(a...);
+        os << std::flush;
+    }
+} print(std::cout), debug(std::cerr);
+
+//! @brief cin高速化など
+inline void fastio() {
+    cin.tie(nullptr);
+    std::ios::sync_with_stdio(false);
+    cout << std::fixed << std::setprecision(11);
+    std::cerr << std::boolalpha;
+}
+}  // namespace bys
+#line 3 "/home/bayashi/dev/byslib/core/macro.hpp"
+
+// clang-format off
+#ifdef LOCAL
+//! @brief デバッグ用出力 ジャッジ上では何もしない。
+#define DEBUG(...) debug("[debug] line:", __LINE__, "\t", __VA_ARGS__)
+#else
+#define DEBUG(...)
+#endif
+//! @brief printしてreturnする。
+#define EXIT(...) { print(__VA_ARGS__); return; }
+// clang-format on
+#line 3 "/home/bayashi/dev/byslib/core/solver.hpp"
+
+namespace bys {
+struct Solver {
+    int IT = 1;
+    Solver() { fastio(); }
+    void solve();
+    void solve(int rep) {
+        for (; IT <= rep; ++IT) solve();
+    }
+};
+}  // namespace bys
+#line 3 "/home/bayashi/dev/byslib/geometry/base.hpp"
+
+namespace bys::geo {
+const ld EPS = 1e-9;
+const ld PI = std::acos(-1.0);
+const ld TAU = PI * 2;
+int sgn(ld a) { return (a < -EPS) ? -1 : (a > EPS) ? 1 : 0; }
+bool isclose(ld a, ld b) { return sgn(a - b) == 0; }
+ld radian(ld degree) { return degree * (PI / 180.0); }
+ld degree(ld theta) { return theta * (180.0 / PI); }
+}  // namespace bys::geo
+#line 3 "/home/bayashi/dev/byslib/geometry/point.hpp"
+
+namespace bys::geo {
+//! @brief 点/ベクトル
+template <class T>
+struct Point {
+    T x, y;
+    Point() : x(0.0), y(0.0) {}
+    Point(T x, T y) : x(x), y(y) {}
+    // clang-format off
+    friend std::istream& operator>>(std::istream& is, Point& v) { return is >> v.x >> v.y; }
+    friend std::ostream& operator<<(std::ostream& os, const Point& v) { return os << v.x << ' ' << v.y; }
+    Point operator+() const { return *this; }
+    Point operator-() const { return Point(-x, -y); }
+    Point operator+(const Point& rh) const { return Point(x + rh.x, y + rh.y); }
+    Point operator-(const Point& rh) const { return Point(x - rh.x, y - rh.y); }
+    Point operator*(const T rh) const { return Point(x * rh, y * rh); }
+    Point operator/(const T rh) const { return Point(x / rh, y / rh); }
+    Point operator+=(const Point& rh) { x += rh.x; y += rh.y; return *this; }
+    Point operator-=(const Point& rh) { x -= rh.x; y -= rh.y; return *this; }
+    Point operator*=(const T rh) { x *= rh; y *= rh; return *this; }
+    Point operator/=(const T rh) { x /= rh; y /= rh; return *this; }
+    bool operator==(const Point& rh) const { return isclose(x, rh.x) && isclose(y, rh.y); }
+    bool operator!=(const Point& rh) const { return !(*this == rh); }
+    // clang-format on
+
+    T norm2() const { return x * x + y * y; }
+    ld norm() const { return std::sqrt(norm2()); }
+    Point normalized() const { return Point(x / norm(), y / norm()); }
+    ld angle() const { return std::atan2(y, x); }
+    Point rotate(ld theta) const {
+        ld ct = std::cos(theta), st = std::sin(theta);
+        return Point(x * ct - y * st, x * st + y * ct);
+    }
+    //! @brief マンハッタン距離用。45度回転して√2倍する
+    Point manhattan_rotate() const { return Point(x - y, x + y); }
+    T dot(const Point& rh) const { return x * rh.x + y * rh.y; }
+    T det(const Point& rh) const { return x * rh.y - y * rh.x; }
+    Point normal() const { return Point(-normalized().y, normalized().x); }
+    Point projection(const Point& to) const { return to * (dot(to) / to.norm2()); }
+    int quadrant() const {
+        if (sgn(y) >= 0) return sgn(x) >= 0 ? 1 : 2;
+        return sgn(x) >= 0 ? 4 : 3;
+    }
+    // 偏角ソート用
+    bool operator<(const Point& rh) const {
+        int q = quadrant(), rhq = rh.quadrant();
+        if (q != rhq) return q < rhq;
+        return sgn(det(rh)) > 0;
+    }
+    bool operator<=(const Point& rh) const {
+        int q = quadrant(), rhq = rh.quadrant();
+        if (q != rhq) return q < rhq;
+        return sgn(det(rh)) >= 0;
+    }
+};
+
+template <class T>
+bool compx(Point<T>& a, Point<T>& b) {
+    return a.x < b.x;
+}
+template <class T>
+bool compy(Point<T>& a, Point<T>& b) {
+    return a.y < b.y;
+}
+
+enum class Turn { Back = -2, CW, Middle, CCW, Front };
+/**
+ * @brief 辺の曲がる方向
+ * @return
+ * +1: CCW ab->bcが左に曲がる
+ * -1: CW  ab->bcが右に曲がる
+ * +2: Front  abの前方
+ * -2: Back   abの後方
+ *  0: Middle ab上
+ */
+template <class T>
+Turn iSP(const Point<T>& a, const Point<T>& b, const Point<T>& c) {
+    int flg = sgn((b - a).det(c - a));
+    if (flg == 1) {
+        return Turn::CCW;
+    } else if (flg == -1) {
+        return Turn::CW;
+    } else {
+        if (sgn((b - a).dot(c - b)) > 0) {
+            return Turn::Front;
+        } else if (sgn((a - b).dot(c - a)) > 0) {
+            return Turn::Back;
+        } else {
+            return Turn::Middle;
+        }
+    }
+}
+/**
+ * -1: Obtuse 鈍角
+ *  0: Right 直角
+ * +1: Acute 鋭角
+ */
+enum class Angle { Obtuse = -1, Right, Acute };
+/**
+ * @brief 角の種類
+ * @return Angle
+ * -1: Obtuse 鈍角
+ *  0: Right 直角
+ * +1: Acute 鋭角
+ */
+template <class T>
+Angle angle_type(const Point<T>& a, const Point<T>& b, const Point<T>& c) {
+    int t = sgn((a - b).dot(c - b));
+    if (t == -1) {
+        return Angle::Obtuse;
+    } else if (t == 0) {
+        return Angle::Right;
+    } else {
+        return Angle::Acute;
+    }
+}
+}  // namespace bys::geo
+#line 3 "abc197_d/main.cpp"
+
+namespace bys {
+void Solver::solve() {
+    auto n = input<int>();
+    auto [a, b] = input<geo::Point<ld>, 2>();
+    auto m = (a + b) / 2;
+    ld theta = geo::TAU / n;
+    print((a - m).rotate(theta) + m);
+}
+}  // namespace bys
+
+int main() {
+    bys::Solver solver;
+    solver.solve(/* bys::input<int>() */);
+    return 0;
+}
