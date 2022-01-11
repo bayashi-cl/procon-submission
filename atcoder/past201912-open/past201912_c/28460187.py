7694d5ebc2e991595cc228f25b2c8eb5ea714634
Masaki Kobayashi <bayashi.cl@gmail.com>
2022-01-11 13:23:17 +0900

    past201912_c

---
+++ atcoder/past201912-open/past201912_c/28460187.py
@@ -0,0 +1,22 @@
+import functools
+import sys
+import typing
+from typing import Callable, Dict, List, Set, Tuple
+
+
+sys.setrecursionlimit(10 ** 6)
+sinput: Callable[..., str] = sys.stdin.readline
+debug = functools.partial(print, file=sys.stderr)
+MOD: int = 998244353
+INF: float = float("Inf")
+IINF: int = sys.maxsize // 2
+
+
+def main() -> None:
+    l = list(map(int, sinput().split()))
+    l.sort()
+    print(l[-3])
+
+
+if __name__ == "__main__":
+    main()
