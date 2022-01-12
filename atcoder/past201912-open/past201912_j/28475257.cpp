8ed9cf8a9ea77d179a02450e6268358b0ea7914b
Masaki Kobayashi <bayashi.cl@gmail.com>
2022-01-12 15:15:05 +0900

    past201912_j

---
+++ atcoder/past201912-open/past201912_j/28475257.cpp
@@ -0,0 +1,381 @@
+#line 2 "/home/bayashi/dev/byslib/core/stdlib.hpp"
+#ifndef LOCAL
+#define NDEBUG
+#endif
+
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <complex>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+namespace bys {
+using std::array, std::vector, std::string, std::set, std::map, std::pair;
+using std::cin, std::cout, std::endl;
+using std::min, std::max, std::sort, std::reverse, std::abs, std::pow;
+
+// alias
+using ll = long long int;
+using ld = long double;
+using Pa = pair<int, int>;
+using Pall = pair<ll, ll>;
+template <class T>
+using uset = std::unordered_set<T>;
+template <class S, class T>
+using umap = std::unordered_map<S, T>;
+}  // namespace bys
+#line 3 "/home/bayashi/dev/byslib/core/const.hpp"
+
+namespace bys {
+constexpr int MOD = 998244353;
+constexpr int MOD7 = 1000000007;
+constexpr int INF = std::numeric_limits<int>::max() / 2;
+constexpr ll LINF = std::numeric_limits<ll>::max() / 2;
+}  // namespace bys
+#line 3 "/home/bayashi/dev/byslib/core/io.hpp"
+
+namespace bys {
+// pair
+template <class T, class U>
+std::istream& operator>>(std::istream& is, std::pair<T, U>& p) {
+    return is >> p.first >> p.second;
+}
+template <typename T, typename U>
+std::ostream& operator<<(std::ostream& os, const std::pair<T, U>& p) {
+    return os << p.first << " " << p.second;
+}
+
+// STL container
+struct is_container_impl {
+    template <typename T>
+    static auto check(T&& obj) -> decltype(obj.begin(), obj.end(), std::true_type{});
+    template <typename T>
+    static auto check(...) -> std::false_type;
+};
+template <typename T>
+class is_container : public decltype(is_container_impl::check<T>(std::declval<T>())) {};
+
+template <typename C, typename std::enable_if<is_container<C>::value && !std::is_same<C, std::string>::value &&
+                                                  !std::is_same<C, std::wstring>::value,
+                                              std::nullptr_t>::type = nullptr>
+std::ostream& operator<<(std::ostream& os, const C& container) noexcept {
+    if (container.empty()) return os;
+    std::for_each(std::begin(container), std::prev(std::end(container)), [&](auto e) { os << e << ' '; });
+    return os << *std::prev(std::end(container));
+}
+
+template <typename C, typename std::enable_if<is_container<C>::value && !std::is_same<C, std::string>::value &&
+                                                  !std::is_same<C, std::wstring>::value,
+                                              std::nullptr_t>::type = nullptr>
+std::istream& operator>>(std::istream& is, C& container) {
+    std::for_each(std::begin(container), std::end(container), [&](auto&& e) { is >> e; });
+    return is;
+}
+
+// I/O helper
+//! @brief 任意の型を1つ
+template <class T>
+inline T input() {
+    T n;
+    cin >> n;
+    return n;
+}
+//! @brief 任意の型がn要素のvector
+template <class T>
+inline vector<T> input(int n) {
+    vector<T> res(n);
+    cin >> res;
+    return res;
+}
+//! @brief 任意の型がn行m列のvector
+template <class T>
+inline vector<vector<T>> input(int n, int m) {
+    vector res(n, vector<T>(m));
+    cin >> res;
+    return res;
+}
+
+//! @brief 任意の型をN個 受け取りは構造化束縛で
+template <class T, size_t N>
+inline std::array<T, N> input() {
+    std::array<T, N> res;
+    cin >> res;
+    return res;
+}
+//! @brief 2つ以上の異なる型 受け取りは構造化束縛で
+template <class S, class T, class... Us>
+std::tuple<S, T, Us...> input() {
+    std::tuple<S, T, Us...> res;
+    std::apply([](auto&... e) { (cin >> ... >> e); }, res);
+    return res;
+}
+//! @brief 標準入力から代入
+template <class... Ts>
+void cinto(Ts&... args) {
+    (cin >> ... >> args);
+}
+//! @brief pythonのprintっぽい挙動をする
+struct Print {
+    std::ostream& os;
+    string sep = " ", end = "\n";
+    Print(std::ostream& os) : os(os) {}
+    ~Print() { os << std::flush; }
+    void operator()() { os << end; }
+    template <class T>
+    void operator()(const T& a) {
+        os << a << end;
+    }
+    //! @brief 空白区切りで出力
+    template <class T, class... Ts>
+    void operator()(const T& a, const Ts&... b) {
+        os << a;
+        (os << ... << (os << sep, b));
+        os << end;
+    }
+    //! @brief 出力後flush インタラクティブ問題用
+    template <class... Ts>
+    void send(const Ts&... a) {
+        operator()(a...);
+        os << std::flush;
+    }
+} print(std::cout), debug(std::cerr);
+
+//! @brief cin高速化など
+inline void fastio() {
+    cin.tie(nullptr);
+    std::ios::sync_with_stdio(false);
+    cout << std::fixed << std::setprecision(11);
+    std::cerr << std::boolalpha;
+}
+}  // namespace bys
+#line 3 "/home/bayashi/dev/byslib/core/macro.hpp"
+
+// clang-format off
+#ifdef LOCAL
+//! @brief デバッグ用出力 ジャッジ上では何もしない。
+#define DEBUG(...) debug("[debug] line:", __LINE__, "\t", __VA_ARGS__)
+#else
+#define DEBUG(...)
+#endif
+//! @brief printしてreturnする。
+#define EXIT(...) { print(__VA_ARGS__); return; }
+#define CONCAT_IMPL(a, b) a##b
+#define CONCAT(a, b) CONCAT_IMPL(a, b)
+//! @brief [[maybe_unused]]な変数を生成。
+#define UV [[maybe_unused]] auto CONCAT(unused_val_, __LINE__)
+// clang-format on
+#line 3 "/home/bayashi/dev/byslib/core/solver.hpp"
+
+namespace bys {
+struct Solver {
+    int IT = 1;
+    Solver() { fastio(); }
+    void solve();
+    void solve(int rep) {
+        for (; IT <= rep; ++IT) solve();
+    }
+};
+}  // namespace bys
+#line 3 "/home/bayashi/dev/byslib/graph/edge.hpp"
+
+namespace bys {
+struct Edge {
+    int from, to;
+    ll cost;
+
+    //! @brief 重みなし単頂点
+    Edge(int to) : from(-1), to(to), cost(1) {}
+    //! @brief 重み付き単頂点
+    Edge(int to, ll cost) : from(-1), to(to), cost(cost) {}
+    //! @brief 重み付き両頂点
+    Edge(int from, int to, ll cost) : from(from), to(to), cost(cost) {}
+    bool operator<(const Edge& rh) const { return cost < rh.cost; }
+    friend std::ostream& operator<<(std::ostream& os, const Edge& e) { return os << e.from << " - " << e.to << ": " << e.cost; }
+};
+using Adj = vector<vector<Edge>>;
+using EdgeList = vector<Edge>;
+}  // namespace bys
+#line 4 "/home/bayashi/dev/byslib/graph/dijkstra.hpp"
+
+namespace bys {
+struct Dijkstra {
+    int n_node;
+    vector<int> prev;
+    vector<ll> cost;
+
+    Dijkstra(const Adj& graph, int start, ll err_val = -1) : n_node(graph.size()), prev(n_node, -1), cost(n_node, LINF) {
+        search(graph, start);
+        std::replace(cost.begin(), cost.end(), LINF, err_val);
+    }
+
+    void search(const Adj& graph, int start) {
+        using Node = std::pair<ll, int>;
+        std::priority_queue<Node, vector<Node>, std::greater<Node>> que;
+        cost[start] = 0;
+        que.push({0, start});
+        while (!que.empty()) {
+            auto top = que.top();
+            que.pop();
+            if (cost[top.second] < top.first) continue;
+            for (auto&& next : graph[top.second]) {
+                ll next_cost = cost[top.second] + next.cost;
+                if (next_cost < cost[next.to]) {
+                    cost[next.to] = next_cost;
+                    prev[next.to] = top.second;
+                    que.push({next_cost, next.to});
+                }
+            }
+        }
+    }
+    vector<int> path(int to) {
+        assert(to < n_node);
+        vector<int> res;
+        while (to != -1) {
+            res.push_back(to);
+            to = prev[to];
+        }
+        std::reverse(res.begin(), res.end());
+        return res;
+    }
+};
+}  // namespace bys
+#line 2 "/home/bayashi/dev/byslib/utility/change.hpp"
+namespace bys {
+template <class T>
+inline bool chmax(T& a, const T& b) {
+    if (a < b) {
+        a = b;
+        return 1;
+    }
+    return 0;
+}
+template <class T>
+inline bool chmin(T& a, const T& b) {
+    if (b < a) {
+        a = b;
+        return 1;
+    }
+    return 0;
+}
+}  // namespace bys
+#line 3 "/home/bayashi/dev/byslib/utility/grid.hpp"
+
+namespace bys {
+//! @brief グリッド探索管理
+struct Grid {
+    int h, w;
+    Grid(int row, int col) : h(row), w(col) {}
+
+    bool contain(int row, int col) const { return 0 <= row && row < h && 0 <= col && col < w; }
+    int area() const { return h * w; }
+    int index(int row, int col) const {
+        assert(contain(row, col));
+        return row * w + col;
+    }
+    pair<int, int> coord(int idx) const {
+        assert(0 <= idx && idx < area());
+        return {idx / w, idx % w};
+    }
+    vector<pair<int, int>> next(int row, int col, const vector<pair<int, int>> delta) const {
+        assert(contain(row, col));
+        vector<pair<int, int>> res;
+        for (auto [di, dj] : delta) {
+            int ni = row + di;
+            int nj = col + dj;
+            if (contain(ni, nj)) res.push_back({ni, nj});
+        }
+        return res;
+    }
+    //! @brief 右・下
+    vector<pair<int, int>> next2(int row, int col) const { return next(row, col, {{1, 0}, {0, 1}}); }
+    //! @brief 上下左右
+    vector<pair<int, int>> next4(int row, int col) const { return next(row, col, {{1, 0}, {-1, 0}, {0, 1}, {0, -1}}); }
+    //! @brief 8方向
+    vector<pair<int, int>> next8(int row, int col) const {
+        vector<pair<int, int>> delta;
+        for (int di = -1; di <= 1; ++di) {
+            for (int dj = -1; dj <= 1; ++dj) {
+                if (di == 0 && dj == 0) continue;
+                delta.push_back({di, dj});
+            }
+        }
+        return next(row, col, delta);
+    }
+};
+}  // namespace bys
+#line 2 "/home/bayashi/dev/byslib/utility/range.hpp"
+
+namespace bys {
+//! @brief pythonのrangeと同じ挙動
+template <typename T>
+struct Range {
+    Range(T start, T stop, T step = 1) : it(start), stop(stop), step(step), dir(step >= 0 ? 1 : -1) {}
+    Range(T stop) : it(0), stop(stop), step(1), dir(1) {}
+    Range<T> begin() const { return *this; }
+    T end() const { return stop; }
+    bool operator!=(const T val) const { return (val - it) * dir > 0; }
+    void operator++() { it += step; }
+    const T& operator*() const { return it; }
+
+   private:
+    T it;
+    const T stop, step;
+    const int dir;
+
+    friend Range reversed(const Range& r) {
+        auto new_start = (r.stop - r.dir - r.it) / r.step * r.step + r.it;
+        return {new_start, r.it - r.dir, -r.step};
+    }
+};
+}  // namespace bys
+#line 6 "past201912_j/main.cpp"
+
+namespace bys {
+void Solver::solve() {
+    auto [h, w] = input<int, 2>();
+    auto a = input<int>(h, w);
+
+    Grid grid(h, w);
+    Adj graph(grid.area());
+    for (int i : Range(h)) {
+        for (int j : Range(w)) {
+            for (auto&& [ni, nj] : grid.next4(i, j)) {
+                graph[grid.index(i, j)].push_back({grid.index(ni, nj), a[ni][nj]});
+            }
+        }
+    }
+    Dijkstra dij_dl(graph, grid.index(h - 1, 0));
+    Dijkstra dij_dr(graph, grid.index(h - 1, w - 1));
+    Dijkstra dij_ur(graph, grid.index(0, w - 1));
+
+    ll ans = LINF;
+    for (int i : Range(h)) {
+        for (int j : Range(w)) {
+            int idx = grid.index(i, j);
+            ll cost = dij_dl.cost[idx] + dij_dr.cost[idx] + dij_ur.cost[idx] - 2 * a[i][j];
+            chmin(ans, cost);
+        }
+    }
+    print(ans);
+}
+}  // namespace bys
+
+int main() {
+    bys::Solver solver;
+    solver.solve(/* bys::input<int>() */);
+    return 0;
+}
