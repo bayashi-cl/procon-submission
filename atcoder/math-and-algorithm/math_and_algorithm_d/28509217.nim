9b1a84ff5fa3ad124cbec50b368c94016337036e
Masaki Kobayashi <bayashi.cl@gmail.com>
2022-01-14 22:37:53 +0900

    math_and_algorithm_d

---
+++ atcoder/math-and-algorithm/math_and_algorithm_d/28509217.nim
@@ -0,0 +1,24 @@
+{.warning[UnusedImport]: off.}
+import macros
+import strutils
+import sequtils
+import strformat
+import math
+import sugar
+
+macro toTuple*(arr: auto, cnt: static[int]): auto =
+  let t = genSym()
+  result = quote do:
+    (let `t` = `arr`; ())
+  for i in 0..<cnt:
+    result[1].add(quote do: `t`[`i`])
+template times*(n: int, body: untyped): untyped = (for _ in 0..<n: body)
+proc getInt(): int {.used.} = stdin.readLine.parseInt
+proc getInts(): seq[int] {.used.} = stdin.readLine.split.map(parseInt)
+
+proc main() =
+  let (a, b, c) = getInts().toTuple(3)
+  echo(a * b * c)
+
+when is_main_module:
+  main()
