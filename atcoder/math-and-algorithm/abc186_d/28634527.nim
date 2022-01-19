e4ae29d067e368e8b11cda951a6e3cc794b538c6
Masaki Kobayashi <bayashi.cl@gmail.com>
2022-01-19 20:20:53 +0900

    abc186_d

---
+++ atcoder/math-and-algorithm/abc186_d/28634527.nim
@@ -0,0 +1,43 @@
+{.warning[UnusedImport]: off.}
+import macros, sugar
+import math, bitops
+import sequtils, strutils, strformat, algorithm
+import sets, tables, deques
+
+macro toTuple*(arr: auto, cnt: static[int]): auto =
+  let t = genSym()
+  result = quote do: (let `t` = `arr`; ())
+  for i in 0..<cnt: result[1].add(quote do: `t`[`i`])
+template times*(n: int, body: untyped): untyped = (for _ in 0..<n: body)
+template `max=`*(x, y: typed): void = x = max(x, y)
+template `min=`*(x, y: typed): void = x = min(x, y)
+template `mod=`*(x, y: typed): void = x = x mod y
+proc getInt(): int {.used.} = stdin.readLine.parseInt
+proc getInts(): seq[int] {.used.} = stdin.readLine.split.map(parseInt)
+proc print*[Ty](args: varargs[Ty, `$`]) =
+  var s = args.join(" ")
+  s.add('\n')
+  stdout.write(s)
+proc debug*[Ty](args: varargs[Ty, `$`]) =
+  var s = args.join(" ")
+  s.add('\n')
+  stderr.write(s)
+const Mod* = 1000000007
+let read* = iterator: string {.closure.} =
+  while true:
+    for s in stdin.readLine.split:
+      yield s
+
+proc main() =
+  let n = getInt()
+  var a = getInts()
+  a.sort()
+  let cs = a.cumsummed()
+
+  var ans = 0
+  for i in 0..<n-1:
+    ans += (cs[n-1] - cs[i]) - a[i] * (n-i-1)
+  print(ans)
+
+when is_main_module:
+  main()
