1098c022eda878d7d5f915a6ef5a4fb3538932cc
Masaki Kobayashi <bayashi.cl@gmail.com>
2022-01-19 22:07:00 +0900

    math_and_algorithm_bo

---
+++ atcoder/math-and-algorithm/math_and_algorithm_bo/28636398.nim
@@ -0,0 +1,45 @@
+{.warning[UnusedImport]: off.}
+import macros, sugar
+import math, bitops
+import sequtils, strutils, strformat, algorithm
+import sets, tables, deques
+
+macro toTuple*(arr: auto, cnt: static[int]): auto =
+  let t = genSym()
+  result = quote do: (let `t` = `arr`; ())
+  for i in 0..<cnt: result[1].add(quote do: `t`[`i`])
+template times*(n: int, body: untyped): untyped = (for _ in 0..<n: body)
+template `max=`*(x, y: typed): void = x = max(x, y)
+template `min=`*(x, y: typed): void = x = min(x, y)
+template `mod=`*(x, y: typed): void = x = x mod y
+proc getInt(): int {.used.} = stdin.readLine.parseInt
+proc getInts(): seq[int] {.used.} = stdin.readLine.split.map(parseInt)
+proc print*[Ty](args: varargs[Ty, `$`]) =
+  var s = args.join(" ")
+  s.add('\n')
+  stdout.write(s)
+proc debug*[Ty](args: varargs[Ty, `$`]) =
+  var s = args.join(" ")
+  s.add('\n')
+  stderr.write(s)
+const Mod* = 1000000007
+const IInf* = int.high div 2
+let read* = iterator: string {.closure.} =
+  while true:
+    for s in stdin.readLine.split:
+      yield s
+
+proc main() =
+  let n, x, y = read().parseInt()
+  for a in 1..n:
+    for b in 1..n:
+      for c in 1..n:
+        let d = x - (a + b + c)
+        if d < 1 or n < d: continue
+        if a * b * c * d == y:
+          print("Yes")
+          return
+  print("No")
+
+when is_main_module:
+  main()
