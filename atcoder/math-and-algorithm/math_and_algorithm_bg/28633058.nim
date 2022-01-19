10ab10226fe2f0cb71c36f5d9febafbcb5f98191
Masaki Kobayashi <bayashi.cl@gmail.com>
2022-01-19 18:37:06 +0900

    math_and_algorithm_bg

---
+++ atcoder/math-and-algorithm/math_and_algorithm_bg/28633058.nim
@@ -0,0 +1,49 @@
+{.warning[UnusedImport]: off.}
+import macros, sugar
+import math, bitops
+import sequtils, strutils, strformat, algorithm
+import sets, tables, deques
+
+{.passc: "-I/opt/ac-library".}
+# {.passc: "-I~/git/ac-library".}
+proc powMod*(a, b, c: int): int {.importcpp: "atcoder::pow_mod(#, @)", header: "<atcoder/math>".}
+
+
+macro toTuple*(arr: auto, cnt: static[int]): auto =
+  let t = genSym()
+  result = quote do: (let `t` = `arr`; ())
+  for i in 0..<cnt: result[1].add(quote do: `t`[`i`])
+template times*(n: int, body: untyped): untyped = (for _ in 0..<n: body)
+template `max=`*(x, y: typed): void = x = max(x, y)
+template `min=`*(x, y: typed): void = x = min(x, y)
+template `mod=`*(x, y: typed): void = x = x mod y
+proc getInt(): int {.used.} = stdin.readLine.parseInt
+proc getInts(): seq[int] {.used.} = stdin.readLine.split.map(parseInt)
+proc print*[Ty](args: varargs[Ty, `$`]) =
+  var s = args.join(" ")
+  s.add('\n')
+  stdout.write(s)
+proc debug*[Ty](args: varargs[Ty, `$`]) =
+  var s = args.join(" ")
+  s.add('\n')
+  stderr.write(s)
+const Mod* = 1000000007
+let read* = iterator: string {.closure.} =
+  while true:
+    for s in stdin.readLine.split:
+      yield s
+
+proc main() =
+  let
+    _ = getInt()
+    a = getInts()
+
+  var ans = 0
+  for i, ai in a:
+    ans += ai * powMod(2, i, Mod) mod Mod
+    ans.mod = Mod
+
+  print(ans)
+
+when is_main_module:
+  main()
