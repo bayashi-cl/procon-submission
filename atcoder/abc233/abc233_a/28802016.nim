0efcf81b2141cf3ea12c904cc3a8e94ffb6e8723
Masaki Kobayashi <bayashi.cl@gmail.com>
2022-01-26 09:46:54 +0900

    abc233_a

---
+++ atcoder/abc233/abc233_a/28802016.nim
@@ -0,0 +1,53 @@
+{.warning[UnusedImport]: off.}
+import macros, sugar
+import math, bitops
+import sequtils, strutils, strformat, algorithm
+import sets, tables, deques
+
+macro toTuple*(arr: auto, cnt: static[int]): auto =
+  let t = genSym()
+  result = quote do: (let `t` = `arr`; ())
+  for i in 0..<cnt: result[1].add(quote do: `t`[`i`])
+template times*(n: int, body: untyped): untyped = (for _ in 0..<n: body)
+proc chmax*[T](a: var T; b: T): bool{.discardable.} = result = if a < b: a = b; true else: false
+proc chmin*[T](a: var T; b: T): bool{.discardable.} = result = if a > b: a = b; true else: false
+proc `|`*[T: SomeInteger](a, b: T): T = result = a or b
+proc `&`*[T: SomeInteger](a, b: T): T = result = a and b
+proc `%`*[T: SomeInteger](a, b: T): T = result = a mod b
+proc `//`*[T: SomeInteger](a, b: T): T = result = a div b
+proc `<<`*[T: SomeInteger](a, b: T): T = result = a shl b
+proc `>>`*[T: SomeInteger](a, b: T): T = result = a shr b
+proc `|=`*[T: SomeInteger](a: var T, b: T) = a = a or b
+proc `&=`*[T: SomeInteger](a: var T, b: T) = a = a and b
+proc `%=`*[T: SomeInteger](a: var T, b: T) = a = a mod b
+proc `//=`*[T: SomeInteger](a: var T, b: T) = a = a div b
+proc `<<=`*[T: SomeInteger](a: var T, b: T) = a = a shl b
+proc `>>=`*[T: SomeInteger](a: var T, b: T) = a = a shr b
+proc getInt*(): int = stdin.readLine.parseInt
+proc getInts*(): seq[int] = stdin.readLine.split.map(parseInt)
+proc print*[Ty](args: varargs[Ty, `$`]) =
+  var s = args.join(" ")
+  s.add('\n')
+  stdout.write(s)
+proc debug*[Ty](args: varargs[Ty, `$`]) =
+  var s = args.join(" ")
+  s.add('\n')
+  stderr.write(s)
+const Mod* = 1000000007
+const IInf* = int.high div 2
+let read* = iterator: string {.closure.} =
+  while true:
+    for s in stdin.readLine.split:
+      yield s
+
+proc ceilDiv*[T: SomeInteger](x, y: T): T {.inline.} =
+  result = x div y
+  if not (x < 0 xor y < 0) and x mod y != 0:
+    inc result
+
+proc main() =
+  let x, y = read().parseInt()
+  print(ceilDiv(max(y - x, 0), 10))
+
+when is_main_module:
+  main()
