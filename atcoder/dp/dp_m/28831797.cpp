e6d2484ca8f00d59eeee175abbc7f7a8637c6c7b
Masaki Kobayashi <bayashi.cl@gmail.com>
2022-01-28 06:11:52 +0900

    dp_m

---
+++ atcoder/dp/dp_m/28831797.cpp
@@ -0,0 +1,437 @@
+#line 2 "/home/bayashi/dev/byslib/acl/acl.hpp"
+#include <iostream>
+#include <atcoder/math>
+#include <atcoder/modint>
+
+namespace bys {
+using atcoder::pow_mod, atcoder::inv_mod;
+using mint = atcoder::modint998244353;
+using mint7 = atcoder::modint1000000007;
+template <int MOD>
+inline std::istream& operator>>(std::istream& is, atcoder::static_modint<MOD>& m) {
+    long long int n;
+    is >> n;
+    m = n;
+    return is;
+}
+template <typename T, typename std::enable_if_t<atcoder::internal::is_modint<T>::value, std::nullptr_t> = nullptr>
+std::ostream& operator<<(std::ostream& os, const T& m) {
+    return os << m.val();
+}
+}  // namespace bys
+#line 2 "/home/bayashi/dev/byslib/core/stdlib.hpp"
+#ifndef LOCAL
+#define NDEBUG
+#endif
+
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <complex>
+#include <iomanip>
+#line 13 "/home/bayashi/dev/byslib/core/stdlib.hpp"
+#include <iterator>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+namespace bys {
+using std::array, std::vector, std::string, std::set, std::map, std::pair;
+using std::cin, std::cout, std::endl;
+using std::min, std::max, std::sort, std::reverse, std::abs, std::pow;
+
+// alias
+using ll = long long int;
+using ld = long double;
+using Pa = pair<int, int>;
+using Pall = pair<ll, ll>;
+using ibool = std::int8_t;
+template <class T>
+using uset = std::unordered_set<T>;
+template <class S, class T>
+using umap = std::unordered_map<S, T>;
+}  // namespace bys
+#line 3 "/home/bayashi/dev/byslib/core/const.hpp"
+
+namespace bys {
+constexpr int MOD = 998244353;
+constexpr int MOD7 = 1000000007;
+constexpr int INF = std::numeric_limits<int>::max() / 2;
+constexpr ll LINF = std::numeric_limits<ll>::max() / 2;
+}  // namespace bys
+#line 4 "/home/bayashi/dev/byslib/core/types.hpp"
+#include <utility>
+
+namespace bys {
+template <class, class = void>
+struct has_lshift_to_ostream : std::false_type {};
+template <class T>
+struct has_lshift_to_ostream<T, std::void_t<decltype(std::declval<std::ostream&>() << std::declval<T&>())>> : std::true_type {};
+
+template <class, class = void>
+struct has_rshift_from_istream : std::false_type {};
+template <class T>
+struct has_rshift_from_istream<T, std::void_t<decltype(std::declval<std::istream&>() >> std::declval<T&>())>> : std::true_type {};
+
+template <class T, class = void>
+struct has_tuple_interface : std::false_type {};
+template <class T>
+struct has_tuple_interface<T, std::void_t<decltype(std::tuple_size<T>())>> : std::true_type {};
+
+template <class, class = void>
+struct has_iterator : std::false_type {};
+template <class T>
+struct has_iterator<T, std::void_t<typename T::iterator>> : std::true_type {};
+
+struct Int1 {};
+}  // namespace bys
+#line 4 "/home/bayashi/dev/byslib/core/printer.hpp"
+
+namespace bys {
+struct Printer {
+    Printer(std::ostream& os_) : os(os_) {}
+    ~Printer() { os << std::flush; }
+
+    template <class T>
+    void cat(T&& v) {
+        if constexpr (has_lshift_to_ostream<std::decay_t<T>>::value) {
+            os << v;
+        } else if constexpr (has_iterator<std::decay_t<T>>::value) {
+            string sep2;
+            if constexpr (has_iterator<std::decay_t<typename std::decay_t<T>::value_type>>::value) {
+                sep2 = _end;
+            } else {
+                sep2 = _sep;
+            }
+            if (!std::empty(v)) {
+                for (auto&& itr = std::begin(v); itr != std::prev(std::end(v)); ++itr) {
+                    cat(*itr);
+                    cat(sep2);
+                }
+                cat(*std::prev(std::end(v)));
+            }
+        } else if constexpr (has_tuple_interface<std::decay_t<T>>::value) {
+            print_tuple(std::forward<T>(v), std::make_index_sequence<std::tuple_size_v<std::decay_t<T>>>());
+        } else {
+            static_assert([] { return false; }(), "type error");
+        }
+    }
+    void print() { cat(_end); }
+    template <class T>
+    void print(T&& top) {
+        cat(std::forward<T>(top));
+        cat(_end);
+    }
+    template <class T, class... Ts>
+    void print(T&& top, Ts&&... args) {
+        cat(std::forward<T>(top));
+        cat(_sep);
+        print(std::forward<Ts>(args)...);
+    }
+    template <class... Ts>
+    void operator()(Ts&&... args) {
+        print(std::forward<Ts>(args)...);
+    }
+
+    void flush() { os << std::flush; }
+    template <class... Ts>
+    void send(Ts&&... args) {
+        print(std::forward<Ts>(args)...);
+        flush();
+    }
+
+    Printer set(string sep_ = " ", string end_ = "\n") {
+        _sep = sep_;
+        _end = end_;
+        return *this;
+    }
+    void lf() { cat(_end); }
+
+   private:
+    std::ostream& os;
+    std::string _sep = " ", _end = "\n";
+    template <std::size_t I, class T>
+    inline void print_tuple_element(T&& elem) {
+        if constexpr (I != 0) cat(_sep);
+        cat(std::forward<T>(elem));
+    }
+    template <class Tp, std::size_t... I>
+    inline void print_tuple(Tp&& tp, std::index_sequence<I...>) {
+        (print_tuple_element<I>(std::forward<decltype(std::get<I>(tp))>(std::get<I>(tp))), ...);
+    }
+};
+}  // namespace bys
+#line 4 "/home/bayashi/dev/byslib/core/scanner.hpp"
+
+namespace bys {
+struct Scanner {
+    Scanner(std::istream& is_) : is(is_){};
+
+    template <class... Ts>
+    void scan(Ts&... args) {
+        is_read_from_istream = true;
+        (is >> ... >> args);
+    }
+
+    template <class T, class... Us>
+    decltype(auto) read() {
+        is_read_from_istream = true;
+        if constexpr (sizeof...(Us) == 0) {
+            if constexpr (has_rshift_from_istream<T>::value) {
+                T res;
+                is >> res;
+                return res;
+            } else if constexpr (has_tuple_interface<T>::value) {
+                auto res = read_tuple<T>(std::make_index_sequence<std::tuple_size_v<T>>());
+                return res;
+            } else if constexpr (std::is_same_v<T, Int1>) {
+                int res;
+                is >> res;
+                --res;
+                return res;
+            } else if constexpr (has_iterator<T>::value) {
+                //! TODO: 一行読んでsplit
+                static_assert([] { return false; }(), "NotImplementedError");
+            } else {
+                static_assert([] { return false; }(), "TypeError");
+            }
+        } else {
+            return std::tuple{read<T>(), read<Us>()...};
+        }
+    }
+
+    template <class T, std::size_t N, typename R = std::conditional_t<std::is_same_v<T, Int1>, int, T>>
+    std::array<R, N> read() {
+        std::array<R, N> res;
+        for (auto&& e : res) e = read<T>();
+        return res;
+    }
+
+    template <class T, typename R = std::conditional_t<std::is_same_v<T, Int1>, int, T>>
+    vector<R> readvec(int n) {
+        vector<R> res(n);
+        for (auto&& e : res) e = read<T>();
+        return res;
+    }
+    template <class T, typename R = std::conditional_t<std::is_same_v<T, Int1>, int, T>>
+    vector<vector<R>> readvec(int n, int m) {
+        vector<vector<R>> res(n);
+        for (auto&& e : res) e = readvec<T>(m);
+        return res;
+    }
+
+    template <class Lambda = std::function<int(std::string)>,
+              typename T = std::invoke_result_t<std::decay_t<Lambda>, std::string>>
+    std::vector<T> readln(
+        Lambda f = [](string x) { return std::stoi(x); }, char sep = ' ') {
+        std::string elem;
+        if (is_read_from_istream) std::getline(is, elem);
+        if (!elem.empty()) std::cerr << "\e[33mWarnig!!\e[0m Some inputs were ignored." << std::endl;
+        std::getline(is, elem);
+        std::stringstream ss{elem};
+        std::vector<T> res;
+        while (std::getline(ss, elem, sep)) res.emplace_back(f(elem));
+        return res;
+    }
+
+   private:
+    std::istream& is;
+    bool is_read_from_istream = false;
+    template <class Tp, std::size_t... I>
+    inline decltype(auto) read_tuple(std::index_sequence<I...>) {
+        return Tp{read<typename std::tuple_element_t<I, Tp>>()...};
+    }
+};
+}  // namespace bys
+#line 5 "/home/bayashi/dev/byslib/core/io.hpp"
+
+namespace bys {
+__attribute__((constructor)) void setup_io() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout << std::fixed << std::setprecision(11);
+    std::cerr << std::fixed << std::setprecision(11);
+    std::cerr << std::boolalpha;
+}
+
+Printer print(std::cout), debug(std::cerr);
+Scanner scanner(std::cin);
+}  // namespace bys
+#line 2 "/home/bayashi/dev/byslib/core/macro.hpp"
+// clang-format off
+#ifdef LOCAL
+//! @brief デバッグ用出力 ジャッジ上では何もしない。
+#define DEBUG(...) { std::cerr << "[debug] line" << std::setw(4) << __LINE__ << ": "; debug(__VA_ARGS__); }
+#else
+#define DEBUG(...)
+#endif
+//! @brief printしてreturnする。
+#define EXIT(...) { print(__VA_ARGS__); return; }
+#define CONCAT_IMPL(a, b) a##b
+#define CONCAT(a, b) CONCAT_IMPL(a, b)
+//! @brief [[maybe_unused]]な変数を生成。
+#define UV [[maybe_unused]] auto CONCAT(unused_val_, __LINE__)
+// clang-format on
+#line 2 "/home/bayashi/dev/byslib/core/solver.hpp"
+
+namespace bys {
+struct Solver {
+    int IT = 1;
+    Solver() {}
+    void solve();
+    void solve(int rep) {
+        for (; IT <= rep; ++IT) solve();
+    }
+};
+}  // namespace bys
+#line 3 "/home/bayashi/dev/byslib/data/cumulative_sum.hpp"
+
+namespace bys {
+template <class T>
+struct CumulativeSum {
+    vector<T> data;
+    CumulativeSum(int n) : data(n + 1){};
+    CumulativeSum(const vector<T>& v) : data(v.size() + 1) { std::copy(v.begin(), v.end(), std::next(data.begin())); };
+    void set(int i, int x) {
+        assert(!build);
+        data[i + 1] = x;
+    }
+    void add(int i, int x) {
+        assert(!build);
+        data[i + 1] += x;
+    }
+    void construct() {
+        assert(!build);
+        std::partial_sum(data.begin(), data.end(), data.begin());
+        build = true;
+    }
+    // [l, r)
+    T sum(int l, int r) {
+        assert(build);
+        if (l > r) return 0;
+        return data[r] - data[l];
+    }
+
+   private:
+    bool build = false;
+};
+template <class T>
+struct CumulativeSum2D {
+    vector<vector<T>> data;
+    CumulativeSum2D(int n, int m) : data(n + 1, vector<T>(m + 1)){};
+    CumulativeSum2D(const vector<vector<T>>& v) : data(v.size() + 1, vector<T>(v[0].size() + 1)) {
+        int n = v.size();
+        for (int i = 0; i < n; ++i) {
+            std::copy(v[i].begin(), v[i].end(), std::next(data[i + 1].begin()));
+        }
+    };
+    void set(int i, int j, int x) {
+        assert(!build);
+        data[i + 1][j + 1] = x;
+    }
+    void add(int i, int j, int x) {
+        assert(!build);
+        data[i + 1][j + 1] += x;
+    }
+    T get(int i, int j) const { return data[i + 1][j + 1]; }
+    void construct() {
+        assert(!build);
+        int n = data.size();
+        int m = data[0].size();
+        for (int i = 1; i < n; ++i) {
+            for (int j = 1; j < m; ++j) {
+                data[i][j] += data[i][j - 1] + data[i - 1][j] - data[i - 1][j - 1];
+            }
+        }
+        build = true;
+    }
+    // [si, gi), [sj, gj)
+    T sum(int si, int sj, int gi, int gj) {
+        assert(build);
+        return (data[gi][gj] - data[si][gj] - data[gi][sj] + data[si][sj]);
+    }
+    // [s, g)
+    T sum(pair<int, int> s, pair<int, int> g) { return sum(s.first, s.second, g.first, g.second); }
+
+   private:
+    bool build = false;
+};
+}  // namespace bys
+#line 2 "/home/bayashi/dev/byslib/utility/change.hpp"
+namespace bys {
+template <class T>
+inline bool chmax(T& a, const T& b) {
+    if (a < b) {
+        a = b;
+        return 1;
+    }
+    return 0;
+}
+template <class T>
+inline bool chmin(T& a, const T& b) {
+    if (b < a) {
+        a = b;
+        return 1;
+    }
+    return 0;
+}
+}  // namespace bys
+#line 2 "/home/bayashi/dev/byslib/utility/range.hpp"
+
+namespace bys {
+//! @brief pythonのrangeと同じ挙動
+template <typename T>
+struct Range {
+    Range(T start, T stop, T step = 1) : it(start), stop(stop), step(step), dir(step >= 0 ? 1 : -1) {}
+    Range(T stop) : it(0), stop(stop), step(1), dir(1) {}
+    Range<T> begin() const { return *this; }
+    T end() const { return stop; }
+    bool operator!=(const T val) const { return (val - it) * dir > 0; }
+    void operator++() { it += step; }
+    const T& operator*() const { return it; }
+
+   private:
+    T it;
+    const T stop, step;
+    const int dir;
+
+    friend Range reversed(const Range& r) {
+        auto new_start = (r.stop - r.dir - r.it) / r.step * r.step + r.it;
+        return {new_start, r.it - r.dir, -r.step};
+    }
+};
+}  // namespace bys
+#line 6 "dp_m/main.cpp"
+
+namespace bys {
+void Solver::solve() {
+    auto [n, k] = scanner.read<int, 2>();
+    auto a = scanner.readvec<int>(n);
+    vector dp(n + 1, vector<mint7>(k + 1));
+    dp[0][0] = 1;
+    for (int i : Range(1, n + 1)) {
+        int ai = a[i - 1];
+        CumulativeSum cs(dp[i - 1]);
+        cs.construct();
+        for (int j : Range(k + 1)) {
+            dp[i][j] = cs.sum(max(0, j - ai), j + 1);
+        }
+    }
+    print(dp[n][k]);
+}
+}  // namespace bys
+
+int main() {
+    bys::Solver solver;
+    solver.solve(/* bys::scanner.read<int>() */);
+    return 0;
+}
