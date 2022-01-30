e36947b615d3b20f1c8fe9d4951f4014ce37d829
Masaki Kobayashi <bayashi.cl@gmail.com>
2022-01-31 05:49:29 +0900

    abc009_1

---
+++ atcoder/abc009/abc009_1/28964488.py
@@ -0,0 +1,21 @@
+import functools
+import sys
+import typing
+from typing import Callable, Dict, List, Set, Tuple
+
+
+sys.setrecursionlimit(10 ** 6)
+sinput: Callable[..., str] = sys.stdin.readline
+debug = functools.partial(print, file=sys.stderr)
+MOD: int = 998244353
+INF: float = float("Inf")
+IINF: int = sys.maxsize // 2
+
+
+def main() -> None:
+    n = int(sinput())
+    print((n + 2 - 1) // 2)
+
+
+if __name__ == "__main__":
+    main()
