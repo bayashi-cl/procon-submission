d8a645453aed2e4ba583ed0cedc67c05a8f247a9
Masaki Kobayashi <bayashi.cl@gmail.com>
2022-02-05 21:03:03 +0900

    abc238_a

---
+++ atcoder/abc238/abc238_a/29065420.py
@@ -0,0 +1,25 @@
+import functools
+import sys
+import typing
+from math import log2
+from typing import Callable, Dict, List, Set, Tuple
+
+
+sys.setrecursionlimit(10 ** 6)
+sinput: Callable[..., str] = sys.stdin.readline
+debug = functools.partial(print, file=sys.stderr)
+MOD: int = 998244353
+INF: float = float("Inf")
+IINF: int = sys.maxsize // 2
+
+
+def main() -> None:
+    n = int(sinput())
+    if n > 2 * log2(n):
+        print("Yes")
+    else:
+        print("No")
+
+
+if __name__ == "__main__":
+    main()
