1429383a6397dd92d553f729969537aa382603c9
Masaki Kobayashi <bayashi.cl@gmail.com>
2022-02-25 14:54:00 +0900

    abc131_e

---
+++ atcoder/abc131/abc131_e/29633587.py
@@ -0,0 +1,86 @@
+import sys
+from itertools import combinations
+
+# from byslib.core.const import IINF, MOD
+from types import ModuleType
+
+byslib = ModuleType("byslib")
+byslib.core = ModuleType("byslib.core")
+byslib.core.const = ModuleType("byslib.core.const")
+
+_code_byslib = """
+\"""
+procon library by bayashi-cl
+github repository: https://github.com/bayashi-cl/byslib-python
+
+This library can be expanded with expander.
+ - https://github.com/bayashi-cl/expander
+\"""
+
+__version__ = "0.0.1"
+"""
+exec(_code_byslib, byslib.__dict__)
+
+_code_byslib_core = """
+
+"""
+exec(_code_byslib_core, byslib.core.__dict__)
+
+_code_byslib_core_const = """
+import sys
+
+MOD: int = 998244353
+MOD7: int = 1000000007
+INF: float = float("Inf")
+IINF: int = sys.maxsize // 2
+"""
+exec(_code_byslib_core_const, byslib.core.const.__dict__)
+
+IINF = byslib.core.const.IINF
+MOD = byslib.core.const.MOD
+# from byslib.core.io import debug, readline, sinput
+byslib.core.io = ModuleType("byslib.core.io")
+
+_code_byslib_core_io = """
+import sys
+from functools import partial
+
+readline = sys.stdin.buffer.readline
+debug = partial(print, file=sys.stderr)
+
+
+def sinput() -> str:
+    return readline().decode().rstrip()
+
+
+def int1(s: str) -> int:
+    return int(s) - 1
+"""
+exec(_code_byslib_core_io, byslib.core.io.__dict__)
+
+debug = byslib.core.io.debug
+readline = byslib.core.io.readline
+sinput = byslib.core.io.sinput
+
+
+def main() -> None:
+    n, k = map(int, readline().split())
+    ma = (n - 1) * (n - 2) // 2
+    if k > ma:
+        print(-1)
+        return
+    ans = []
+    for i in range(1, n):
+        ans.append((1, i + 1))
+
+    for u, v in list(combinations(range(1, n), 2))[: ma - k]:
+        ans.append((u + 1, v + 1))
+
+    print(len(ans))
+    for e in ans:
+        print(*e)
+
+
+if __name__ == "__main__":
+    sys.setrecursionlimit(10**6)
+    main()
