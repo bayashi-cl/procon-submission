d0b82ddecae1752a17bb6973293821c405ac1131
Masaki Kobayashi <bayashi.cl@gmail.com>
2022-02-27 21:04:46 +0900

    arc136_a

---
+++ atcoder/arc136/arc136_a/29740768.py
@@ -0,0 +1,84 @@
+import sys
+
+# from byslib.core.const import IINF, MOD
+from types import ModuleType
+
+byslib = ModuleType("byslib")
+byslib.core = ModuleType("byslib.core")
+byslib.core.const = ModuleType("byslib.core.const")
+
+_code_byslib = """
+\"""
+procon library by bayashi-cl
+github repository: https://github.com/bayashi-cl/byslib-python
+
+This library can be expanded with expander.
+ - https://github.com/bayashi-cl/expander
+\"""
+
+__version__ = "0.0.1"
+"""
+exec(_code_byslib, byslib.__dict__)
+
+_code_byslib_core = """
+
+"""
+exec(_code_byslib_core, byslib.core.__dict__)
+
+_code_byslib_core_const = """
+import sys
+
+MOD: int = 998244353
+MOD7: int = 1000000007
+INF: float = float("Inf")
+IINF: int = sys.maxsize // 2
+"""
+exec(_code_byslib_core_const, byslib.core.const.__dict__)
+
+IINF = byslib.core.const.IINF
+MOD = byslib.core.const.MOD
+# from byslib.core.io import debug, readline, sinput
+byslib.core.io = ModuleType("byslib.core.io")
+
+_code_byslib_core_io = """
+import sys
+from functools import partial
+
+readline = sys.stdin.buffer.readline
+debug = partial(print, file=sys.stderr)
+
+
+def sinput() -> str:
+    return readline().decode().rstrip()
+
+
+def int1(s: str) -> int:
+    return int(s) - 1
+"""
+exec(_code_byslib_core_io, byslib.core.io.__dict__)
+
+debug = byslib.core.io.debug
+readline = byslib.core.io.readline
+sinput = byslib.core.io.sinput
+
+
+def main() -> None:
+    _ = int(readline())
+    s = sinput()
+    s = s.replace("A", "BB")
+    ans = []
+    for si in s:
+        if si == "B":
+            if ans and ans[-1] == "B":
+                ans[-1] = "A"
+            else:
+                ans.append("B")
+        else:
+            ans.append(si)
+
+    print("".join(ans))
+
+
+if __name__ == "__main__":
+    sys.setrecursionlimit(10**6)
+    main()
