60180c1481e2e15216f4cce3492173f8e46e5246
Masaki Kobayashi <bayashi.cl@gmail.com>
2022-03-09 01:01:01 +0900

    D: Twist the Permutation

---
+++ codeforces/1650/D/148885322.py
@@ -0,0 +1,88 @@
+import sys
+from typing import List
+
+# from byslib.core.const import IINF, MOD
+from types import ModuleType
+
+byslib = ModuleType("byslib")
+byslib.core = ModuleType("byslib.core")
+byslib.core.const = ModuleType("byslib.core.const")
+
+_code_byslib = """
+\"""
+procon library by bayashi-cl
+github repository: https://github.com/bayashi-cl/byslib-python
+
+This library can be expanded with expander.
+ - https://github.com/bayashi-cl/expander
+\"""
+
+__version__ = "0.0.1"
+"""
+exec(_code_byslib, byslib.__dict__)
+
+_code_byslib_core = """
+
+"""
+exec(_code_byslib_core, byslib.core.__dict__)
+
+_code_byslib_core_const = """
+import sys
+
+MOD: int = 998244353
+MOD7: int = 1000000007
+INF: float = float("Inf")
+IINF: int = sys.maxsize // 2
+"""
+exec(_code_byslib_core_const, byslib.core.const.__dict__)
+
+IINF = byslib.core.const.IINF
+MOD = byslib.core.const.MOD
+# from byslib.core.io import debug, readline, sinput, int1
+byslib.core.io = ModuleType("byslib.core.io")
+
+_code_byslib_core_io = """
+import sys
+from functools import partial
+
+readline = sys.stdin.buffer.readline
+debug = partial(print, file=sys.stderr)
+
+
+def sinput() -> str:
+    return readline().decode().rstrip()
+
+
+def int1(s: str) -> int:
+    return int(s) - 1
+"""
+exec(_code_byslib_core_io, byslib.core.io.__dict__)
+
+debug = byslib.core.io.debug
+readline = byslib.core.io.readline
+sinput = byslib.core.io.sinput
+int1 = byslib.core.io.int1
+
+
+def main() -> None:
+    n = int(readline())
+    a = list(map(int1, readline().split()))
+    pos = [0] * n
+    for i, ai in enumerate(a):
+        pos[ai] = i
+
+    ans: List = []
+    for i in reversed(range(n)):
+        s = (pos[i] - i) % (i + 1)
+        ans.append(s)
+        for j in range(n):
+            pos[j] = (pos[j] - s) % (i + 1)
+
+    print(*reversed(ans))
+
+
+if __name__ == "__main__":
+    sys.setrecursionlimit(10**6)
+    t = int(readline())
+    for _ in range(t):
+        main()
