5c0708893caa12d5e1d4616ddbd8671be7d2efb7
Masaki Kobayashi <bayashi.cl@gmail.com>
2022-03-13 15:00:13 +0900

    abc243_g

---
+++ atcoder/abc243/abc243_g/30098963.py
@@ -0,0 +1,108 @@
+from math import isqrt
+
+# from byslib.core.config import procon_setup
+from types import ModuleType
+
+byslib = ModuleType("byslib")
+byslib.core = ModuleType("byslib.core")
+byslib.core.config = ModuleType("byslib.core.config")
+
+_code_byslib = """
+\"""
+procon library by bayashi-cl
+github repository: https://github.com/bayashi-cl/byslib-python
+
+This library can be expanded with expander.
+ - https://github.com/bayashi-cl/expander
+\"""
+
+__version__ = "0.0.1"
+"""
+exec(_code_byslib, byslib.__dict__)
+
+_code_byslib_core = """
+
+"""
+exec(_code_byslib_core, byslib.core.__dict__)
+
+_code_byslib_core_config = """
+import sys
+from typing import Callable
+
+
+def procon_setup(main: Callable[..., None]) -> Callable[..., None]:
+    sys.setrecursionlimit(10**7)
+
+    def wrapper(case: int = 1) -> None:
+        for i in range(case):
+            main(case=i + 1)
+
+    return wrapper
+"""
+exec(_code_byslib_core_config, byslib.core.config.__dict__)
+
+procon_setup = byslib.core.config.procon_setup
+# from byslib.core.const import IINF, MOD
+byslib.core.const = ModuleType("byslib.core.const")
+
+_code_byslib_core_const = """
+import sys
+
+MOD: int = 998244353
+MOD7: int = 1000000007
+INF: float = float("Inf")
+IINF: int = sys.maxsize // 2
+"""
+exec(_code_byslib_core_const, byslib.core.const.__dict__)
+
+IINF = byslib.core.const.IINF
+MOD = byslib.core.const.MOD
+# from byslib.core.fastio import debug, readline, sinput
+byslib.core.fastio = ModuleType("byslib.core.fastio")
+
+_code_byslib_core_fastio = """
+import io
+import os
+import sys
+from functools import partial
+
+readline = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+debug = partial(print, file=sys.stderr)
+
+
+def sinput() -> str:
+    return readline().decode().rstrip()
+
+
+def int1(s: str) -> int:
+    return int(s) - 1
+"""
+exec(_code_byslib_core_fastio, byslib.core.fastio.__dict__)
+
+debug = byslib.core.fastio.debug
+readline = byslib.core.fastio.readline
+sinput = byslib.core.fastio.sinput
+
+
+@procon_setup
+def main(**kwargs) -> None:
+    x = int(readline())
+    sqx = isqrt(x)
+    dp = [0] * (isqrt(sqx) + 1)
+    for i in range(1, len(dp)):
+        dp[i] = sqx - i**2 + 1 + dp[i - 1]
+
+    while len(dp) > 2:
+        dp2 = [0] * (isqrt(len(dp)) + 1)
+        back = dp[-1]
+        for i in range(1, len(dp2)):
+            dp2[i] = back - dp[i**2 - 1] + dp2[i - 1]
+        dp = dp2
+
+    print(dp[1])
+
+
+if __name__ == "__main__":
+    # t = 1
+    t = int(readline())
+    main(t)
