a8d034944c50f30162850420cafa723269cf8595
Masaki Kobayashi <bayashi.cl@gmail.com>
2022-04-02 21:24:17 +0900

    abc246_d

---
+++ atcoder/abc246/abc246_d/30643069.py
@@ -0,0 +1,157 @@
+# from byslib.core.config import procon_setup
+from types import ModuleType
+
+byslib = ModuleType("byslib")
+byslib.core = ModuleType("byslib.core")
+byslib.core.config = ModuleType("byslib.core.config")
+
+_code_byslib = """
+\"""
+procon library by bayashi-cl
+github repository: https://github.com/bayashi-cl/byslib-python
+
+This library can be expanded with expander.
+ - https://github.com/bayashi-cl/expander
+\"""
+
+__version__ = "0.0.2"
+"""
+exec(_code_byslib, byslib.__dict__)
+
+_code_byslib_core = """
+
+"""
+exec(_code_byslib_core, byslib.core.__dict__)
+
+_code_byslib_core_config = """
+import sys
+from typing import Callable
+
+
+def procon_setup(main: Callable[..., None]) -> Callable[..., None]:
+    sys.setrecursionlimit(10**7)
+
+    def wrapper(case: int = 1) -> None:
+        for i in range(case):
+            main(case=i + 1)
+
+    return wrapper
+"""
+exec(_code_byslib_core_config, byslib.core.config.__dict__)
+
+procon_setup = byslib.core.config.procon_setup
+# from byslib.core.const import IINF, INF, MOD
+byslib.core.const = ModuleType("byslib.core.const")
+
+_code_byslib_core_const = """
+import sys
+
+MOD: int = 998244353
+MOD7: int = 1000000007
+INF: float = float("Inf")
+IINF: int = sys.maxsize // 2
+"""
+exec(_code_byslib_core_const, byslib.core.const.__dict__)
+
+IINF = byslib.core.const.IINF
+INF = byslib.core.const.INF
+MOD = byslib.core.const.MOD
+# from byslib.core.fastio import debug, readline, sinput
+byslib.core.fastio = ModuleType("byslib.core.fastio")
+
+_code_byslib_core_fastio = """
+import io
+import os
+import sys
+from functools import partial
+from typing import Union
+
+readline = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+debug = partial(print, file=sys.stderr)
+
+
+def sinput() -> str:
+    return readline().decode().rstrip()
+
+
+def int1(s: Union[str, bytes]) -> int:
+    return int(s) - 1
+"""
+exec(_code_byslib_core_fastio, byslib.core.fastio.__dict__)
+
+debug = byslib.core.fastio.debug
+readline = byslib.core.fastio.readline
+sinput = byslib.core.fastio.sinput
+# from byslib.utility.binary_search import meguru_bisect
+byslib.utility = ModuleType("byslib.utility")
+byslib.utility.binary_search = ModuleType("byslib.utility.binary_search")
+
+_code_byslib_utility = """
+
+"""
+exec(_code_byslib_utility, byslib.utility.__dict__)
+
+_code_byslib_utility_binary_search = """
+from typing import Callable
+
+
+def meguru_bisect(ok: int, ng: int, is_ok: Callable[..., bool], *args) -> int:
+    \"""二分探索法
+    Args:
+        ok (int): is_okを満たす初期値
+        ng (int): is_okを満たさない初期値
+        is_ok (typing.Callable[..., bool]): 判定用関数
+        *args (object): is_okに渡される引数
+
+    Returns:
+        int: is_okを満たす最大/小の整数
+    \"""
+    assert is_ok(ok, *args)
+    assert not is_ok(ng, *args)
+
+    while abs(ok - ng) > 1:
+        mid = (ok + ng) >> 1
+        if is_ok(mid, *args):
+            ok = mid
+        else:
+            ng = mid
+    return ok
+"""
+exec(_code_byslib_utility_binary_search, byslib.utility.binary_search.__dict__)
+
+meguru_bisect = byslib.utility.binary_search.meguru_bisect
+
+
+@procon_setup
+def main(**kwargs) -> None:
+    n = int(readline())
+
+    def ok(a: int, b: int) -> bool:
+        if a < 0:
+            return False
+        return a**3 + a**2 * b + a * b**2 + b**3 >= n
+
+    b = 0
+    ans = INF
+    while b**3 <= n:
+        a = meguru_bisect(10**6, -1, ok, b)
+        ans = min(ans, a**3 + a**2 * b + a * b**2 + b**3)
+        b += 1
+
+    print(ans)
+
+
+if __name__ == "__main__":
+    t = 1
+    # t = int(readline())
+    main(t)
+
+
+# package infomations
+# --------------------------------------------------------------------------
+# byslib-python
+#   Version  : 0.0.2
+#   Author   : bayashi-cl
+#   Home-page: https://bayashi-cl.github.io/byslib-python/
+#   License  : CC0
+# --------------------------------------------------------------------------
