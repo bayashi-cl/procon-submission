58c3c355f8afd6cae1a3d670a972667cee362934
Masaki Kobayashi <bayashi.cl@gmail.com>
2022-07-09 21:05:02 +0900

    abc259_b

---
+++ atcoder/abc259/abc259_b/33075241.cpp
@@ -0,0 +1,559 @@
+/**
+ * @file core.hpp
+ * @author bayashi_cl
+ * @brief core/all
+ *
+ * C++ library for competitive programming by bayashi_cl
+ * Repository: https://github.com/bayashi-cl/byslib
+ * Document  : https://bayashi-cl.github.io/byslib/
+ */
+#ifndef LOCAL
+#define NDEBUG
+#endif
+/**
+ * @file stdlib.hpp
+ * @brief STL Template
+ */
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <complex>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+namespace bys {
+using std::array, std::vector, std::string, std::set, std::map, std::pair;
+using std::cin, std::cout, std::endl;
+using std::min, std::max, std::sort, std::reverse, std::abs, std::pow;
+
+// alias
+using ll = long long int;
+using ld = long double;
+using Pa = pair<int, int>;
+using Pall = pair<ll, ll>;
+using ibool = std::int8_t;
+template <class T>
+using uset = std::unordered_set<T>;
+template <class S, class T>
+using umap = std::unordered_map<S, T>;
+}  // namespace bys
+/**
+ * @file const.hpp
+ * @brief Const
+ */
+namespace bys {
+constexpr int MOD = 998244353;
+constexpr int MOD7 = 1000000007;
+constexpr int INF = std::numeric_limits<int>::max() / 2;
+constexpr ll LINF = std::numeric_limits<ll>::max() / 2;
+}  // namespace bys
+/**
+ * @file types.hpp
+ * @brief Types
+ *
+ * type_traits拡張
+ */
+namespace bys {
+template <class, class = void>
+struct has_lshift_to_ostream : std::false_type {};
+template <class T>
+struct has_lshift_to_ostream<T, std::void_t<decltype(std::declval<std::ostream&>() << std::declval<T&>())>> : std::true_type {};
+
+template <class, class = void>
+struct has_rshift_from_istream : std::false_type {};
+template <class T>
+struct has_rshift_from_istream<T, std::void_t<decltype(std::declval<std::istream&>() >> std::declval<T&>())>> : std::true_type {};
+
+template <class T, class = void>
+struct has_tuple_interface : std::false_type {};
+template <class T>
+struct has_tuple_interface<T, std::void_t<decltype(std::tuple_size<T>())>> : std::true_type {};
+
+template <class, class = void>
+struct has_iterator : std::false_type {};
+template <class T>
+struct has_iterator<T, std::void_t<typename T::iterator>> : std::true_type {};
+
+struct Int1 {};
+}  // namespace bys
+/**
+ * @file printer.hpp
+ * @brief Output
+ */
+namespace bys {
+class Printer {
+    std::ostream& os;
+    std::string _sep = " ", _end = "\n";
+    template <std::size_t I, class T>
+    inline void print_tuple_element(T&& elem) {
+        if constexpr (I != 0) cat(_sep);
+        cat(std::forward<T>(elem));
+    }
+    template <class Tp, std::size_t... I>
+    inline void print_tuple(Tp&& tp, std::index_sequence<I...>) {
+        (print_tuple_element<I>(std::forward<decltype(std::get<I>(tp))>(std::get<I>(tp))), ...);
+    }
+
+   public:
+    Printer(std::ostream& os_) : os(os_) { os << std::fixed << std::setprecision(11) << std::boolalpha; }
+    ~Printer() { os << std::flush; }
+
+    template <class T>
+    void cat(T&& v) {
+        if constexpr (has_lshift_to_ostream<std::decay_t<T>>::value) {
+            os << v;
+        } else if constexpr (has_iterator<std::decay_t<T>>::value) {
+            string sep2;
+            if constexpr (has_iterator<std::decay_t<typename std::decay_t<T>::value_type>>::value) {
+                sep2 = _end;
+            } else {
+                sep2 = _sep;
+            }
+            for (auto &&itr = std::begin(v), end = std::end(v); itr != end; ++itr) {
+                cat(*itr);
+                if (std::next(itr) != end) cat(sep2);
+            }
+        } else if constexpr (has_tuple_interface<std::decay_t<T>>::value) {
+            print_tuple(std::forward<T>(v), std::make_index_sequence<std::tuple_size_v<std::decay_t<T>>>());
+        } else {
+            static_assert([] { return false; }(), "type error");
+        }
+    }
+    void print() { cat(_end); }
+    template <class T>
+    void print(T&& top) {
+        cat(std::forward<T>(top));
+        cat(_end);
+    }
+    template <class T, class... Ts>
+    void print(T&& top, Ts&&... args) {
+        cat(std::forward<T>(top));
+        cat(_sep);
+        print(std::forward<Ts>(args)...);
+    }
+    //! @brief 空白区切りで出力
+    template <class... Ts>
+    void operator()(Ts&&... args) {
+        print(std::forward<Ts>(args)...);
+    }
+
+    void flush() { os << std::flush; }
+    //! @brief 出力後にflush
+    template <class... Ts>
+    void send(Ts&&... args) {
+        print(std::forward<Ts>(args)...);
+        flush();
+    }
+
+    //! @brief 区切り文字と終端文字を設定
+    Printer set(string sep_ = " ", string end_ = "\n") {
+        _sep = sep_;
+        _end = end_;
+        return *this;
+    }
+    void lf() { cat(_end); }
+};
+}  // namespace bys
+/**
+ * @file scanner.hpp
+ * @brief Input
+ */
+namespace bys {
+class Scanner {
+    std::istream& is;
+    template <class Tp, std::size_t... I>
+    inline decltype(auto) read_tuple(std::index_sequence<I...>) {
+        return Tp{read<typename std::tuple_element_t<I, Tp>>()...};
+    }
+
+   public:
+    Scanner(std::istream& is_) : is(is_) { is.tie(nullptr); }
+
+    template <class... Ts>
+    void scan(Ts&... args) {
+        (is >> ... >> args);
+    }
+
+    /**
+     * @brief 2つ以上の異なる型
+     *
+     * 受け取りは構造化束縛で
+     */
+    template <class T, class... Us>
+    decltype(auto) read() {
+        if constexpr (sizeof...(Us) == 0) {
+            if constexpr (has_rshift_from_istream<T>::value) {
+                T res;
+                is >> res;
+                return res;
+            } else if constexpr (has_tuple_interface<T>::value) {
+                auto res = read_tuple<T>(std::make_index_sequence<std::tuple_size_v<T>>());
+                return res;
+            } else if constexpr (std::is_same_v<T, Int1>) {
+                int res;
+                is >> res;
+                --res;
+                return res;
+            } else if constexpr (has_iterator<T>::value) {
+                //! TODO: 一行読んでsplit
+                static_assert([] { return false; }(), "NotImplementedError");
+            } else {
+                static_assert([] { return false; }(), "TypeError");
+            }
+        } else {
+            return std::tuple{read<T>(), read<Us>()...};
+        }
+    }
+    /**
+     * @brief 型TをN個
+     *
+     * 受け取りは構造化束縛で
+     */
+    template <class T, std::size_t N, typename R = std::conditional_t<std::is_same_v<T, Int1>, int, T>>
+    std::array<R, N> read() {
+        std::array<R, N> res;
+        for (auto&& e : res) e = read<T>();
+        return res;
+    }
+    //! @brief n要素のvector
+    template <class T, typename R = std::conditional_t<std::is_same_v<T, Int1>, int, T>>
+    vector<R> readvec(int n) {
+        vector<R> res(n);
+        for (auto&& e : res) e = read<T>();
+        return res;
+    }
+    //! @brief n*m要素の2次元vector
+    template <class T, typename R = std::conditional_t<std::is_same_v<T, Int1>, int, T>>
+    vector<vector<R>> readvec(int n, int m) {
+        vector<vector<R>> res(n);
+        for (auto&& e : res) e = readvec<T>(m);
+        return res;
+    }
+
+    /**
+     * @brief 1行を読み取りそれを要素ごとに変換
+     * @param f 文字列からの変換関数
+     * @param sep 区切り文字
+     */
+    template <class Lambda = std::function<int(std::string)>,
+              typename T = std::invoke_result_t<std::decay_t<Lambda>, std::string>>
+    std::vector<T> readln(
+        Lambda f = [](string x) { return std::stoi(x); }, char sep = ' ') {
+        std::ws(is);
+        std::string elem;
+        std::getline(is, elem);
+        std::stringstream ss{elem};
+        std::vector<T> res;
+        while (std::getline(ss, elem, sep)) res.emplace_back(f(elem));
+        return res;
+    }
+    /**
+     * @brief 1行を文字列で取得
+     * @param skip_ws 先頭の空白・改行を読み飛ばす
+     */
+    std::string getline(bool skip_ws = true) {
+        if (skip_ws) std::ws(is);
+        std::string res;
+        std::getline(is, res);
+        return res;
+    }
+};
+}  // namespace bys
+/**
+ * @file io.hpp
+ * @brief I/O
+ */
+namespace bys {
+template <class... Args>
+std::string debugfmt(int line, Args&&... args) {
+    std::stringstream ss;
+    Printer printer(ss);
+    ss << "📌 line" << std::setw(4) << line << ": ";
+    std::string space = "\n             ";
+    printer.set(" ", space).print(std::forward<Args>(args)...);
+    auto result = ss.str();
+    return result.substr(0, result.length() - space.length());
+}
+
+Printer print(std::cout), debug(std::cerr);
+Scanner scanner(std::cin);
+}  // namespace bys
+/**
+ * @file macro.hpp
+ * @brief Macro
+ */
+// clang-format off
+#ifdef LOCAL
+//! @brief デバッグ用出力 ジャッジ上では何もしない。
+#define DEBUG(...) debug(debugfmt(__LINE__, __VA_ARGS__));
+#else
+#define DEBUG(...)
+#endif
+#define DEBUGCASE(casenum, ...) if (TESTCASE == casenum) DEBUG(__VA_ARGS__);
+//! @brief printしてreturnする。
+#define EXIT(...) { print(__VA_ARGS__); return; }
+#define CONCAT_IMPL(a, b) a##b
+#define CONCAT(a, b) CONCAT_IMPL(a, b)
+//! @brief [[maybe_unused]]な変数を生成。
+#define UV [[maybe_unused]] auto CONCAT(unused_val_, __LINE__)
+#define RE std::runtime_error("line: " + std::to_string(__LINE__) + ", func: " + __func__)
+// clang-format on
+/**
+ * @file solver.hpp
+ * @brief Solver
+ */
+namespace bys {
+struct Solver {
+    static inline int TESTCASE = 1;
+    static void solve();
+    static int main(int t = 1) {
+        std::ios::sync_with_stdio(false);
+
+        for (; TESTCASE <= t; ++TESTCASE) solve();
+
+        if (not std::cin.good()) std::cerr << "🟡 Input failed." << std::endl;
+        if (not std::ws(std::cin).eof()) std::cerr << "🟡 Unused input." << std::endl;
+        return 0;
+    }
+};
+}  // namespace bys
+// -------------------------------------
+/**
+ * @file base.hpp
+ * @brief Base
+ */
+//! @brief 幾何
+namespace bys::geo {
+const ld EPS = 1e-9;
+const ld PI = std::acos(-1.0);
+const ld TAU = PI * 2;
+int sgn(ld a) { return (a < -EPS) ? -1 : (a > EPS) ? 1 : 0; }
+bool isclose(ld a, ld b) { return sgn(a - b) == 0; }
+//! @brief 度数法 -> 弧度法
+ld radian(ld degree) { return degree * (PI / 180.0); }
+//! @brief 弧度法 -> 度数法
+ld degree(ld theta) { return theta * (180.0 / PI); }
+}  // namespace bys::geo
+/**
+ * @file point.hpp
+ * @brief Point
+ */
+namespace bys::geo {
+//! @brief 点/ベクトル
+template <class T>
+struct Point {
+    T x, y;
+    Point() : x(0.0), y(0.0) {}
+    Point(T x, T y) : x(x), y(y) {}
+    // clang-format off
+    friend std::istream& operator>>(std::istream& is, Point& v) { return is >> v.x >> v.y; }
+    friend std::ostream& operator<<(std::ostream& os, const Point& v) { return os << v.x << ' ' << v.y; }
+    Point operator+() const { return *this; }
+    Point operator-() const { return Point(-x, -y); }
+    Point operator+(const Point& rh) const { return Point(x + rh.x, y + rh.y); }
+    Point operator-(const Point& rh) const { return Point(x - rh.x, y - rh.y); }
+    Point operator*(const T rh) const { return Point(x * rh, y * rh); }
+    Point operator/(const T rh) const { return Point(x / rh, y / rh); }
+    Point operator+=(const Point& rh) { x += rh.x; y += rh.y; return *this; }
+    Point operator-=(const Point& rh) { x -= rh.x; y -= rh.y; return *this; }
+    Point operator*=(const T rh) { x *= rh; y *= rh; return *this; }
+    Point operator/=(const T rh) { x /= rh; y /= rh; return *this; }
+    bool operator==(const Point& rh) const { return isclose(x, rh.x) && isclose(y, rh.y); }
+    bool operator!=(const Point& rh) const { return !(*this == rh); }
+    // clang-format on
+
+    //! @brief ノルム^2
+    T norm2() const { return x * x + y * y; }
+    //! @brief ノルム
+    ld norm() const { return std::sqrt(norm2()); }
+    //! @brief 単位ベクトル
+    Point normalized() const { return Point(x / norm(), y / norm()); }
+    //! @brief 偏角
+    ld angle() const { return std::atan2(y, x); }
+    //! @brief 回転
+    Point rotate(ld theta) const {
+        ld ct = std::cos(theta), st = std::sin(theta);
+        return Point(x * ct - y * st, x * st + y * ct);
+    }
+    Point rotate90() const { return Point(-y, x); }
+    //! @brief マンハッタン距離用。45度回転して√2倍する
+    Point manhattan_rotate() const { return Point(x - y, x + y); }
+    //! @brief 内積
+    T dot(const Point& rh) const { return x * rh.x + y * rh.y; }
+    //! @brief 外積
+    T det(const Point& rh) const { return x * rh.y - y * rh.x; }
+    //! @brief 法線ベクトル
+    Point normal() const { return Point(-normalized().y, normalized().x); }
+    //! @brief 正射影ベクトル
+    Point projection(const Point& to) const { return to * (dot(to) / to.norm2()); }
+    //! @brief 象限
+    int quadrant() const {
+        if (sgn(y) >= 0) return sgn(x) >= 0 ? 1 : 2;
+        return sgn(x) >= 0 ? 4 : 3;
+    }
+    //! @brief 偏角ソート用
+    bool operator<(const Point& rh) const {
+        int q = quadrant(), rhq = rh.quadrant();
+        if (q != rhq) return q < rhq;
+        return sgn(det(rh)) > 0;
+    }
+    bool operator<=(const Point& rh) const {
+        int q = quadrant(), rhq = rh.quadrant();
+        if (q != rhq) return q < rhq;
+        return sgn(det(rh)) >= 0;
+    }
+};
+
+template <class T>
+bool compx(Point<T>& a, Point<T>& b) {
+    return a.x < b.x;
+}
+template <class T>
+bool compy(Point<T>& a, Point<T>& b) {
+    return a.y < b.y;
+}
+//! @brief 曲がる方向
+enum class Turn {
+    //! abの後方
+    Back = -2,
+    //! ab->bcが右に曲がる
+    CW = -1,
+    //! ab上
+    Middle = 0,
+    //! ab->bcが左に曲がる
+    CCW = 1,
+    //! abの前方
+    Front = 2,
+};
+//! @brief 辺の曲がる方向
+template <class T>
+Turn iSP(const Point<T>& a, const Point<T>& b, const Point<T>& c) {
+    int flg = sgn((b - a).det(c - a));
+    if (flg == 1) {
+        return Turn::CCW;
+    } else if (flg == -1) {
+        return Turn::CW;
+    } else {
+        if (sgn((b - a).dot(c - b)) > 0) {
+            return Turn::Front;
+        } else if (sgn((a - b).dot(c - a)) > 0) {
+            return Turn::Back;
+        } else {
+            return Turn::Middle;
+        }
+    }
+}
+enum class Angle {
+    //! 鈍角
+    Obtuse = -1,
+    //! 直角
+    Right = 0,
+    //! 鋭角
+    Acute = 1,
+};
+
+//! @brief 角の種類
+template <class T>
+Angle angle_type(const Point<T>& a, const Point<T>& b, const Point<T>& c) {
+    int t = sgn((a - b).dot(c - b));
+    if (t == -1) {
+        return Angle::Obtuse;
+    } else if (t == 0) {
+        return Angle::Right;
+    } else {
+        return Angle::Acute;
+    }
+}
+}  // namespace bys::geo
+/**
+ * @file change.hpp
+ * @brief chmin/chmax
+ */
+namespace bys {
+/**
+ * @brief 最大値で更新
+ * @return true 更新されたとき
+ */
+template <class T>
+inline bool chmax(T& a, const T& b) {
+    return a < b ? a = b, true : false;
+}
+/**
+ * @brief 最小値で更新
+ * @return true 更新されたとき
+ */
+template <class T>
+inline bool chmin(T& a, const T& b) {
+    return a > b ? a = b, true : false;
+}
+}  // namespace bys
+/**
+ * @file range.hpp
+ * @brief Python::range
+ *
+ * Python再現シリーズ range編
+ * See: https://docs.python.org/ja/3/library/stdtypes.html#range
+ */
+namespace bys {
+template <typename T>
+class Range {
+    T it;
+    const T stop, step;
+    const int dir;
+
+   public:
+    Range(T start, T stop, T step = 1) : it(start), stop(stop), step(step), dir(step >= 0 ? 1 : -1) {}
+    Range(T stop) : it(0), stop(stop), step(1), dir(1) {}
+    Range<T> begin() const { return *this; }
+    T end() const { return stop; }
+    bool operator!=(const T val) const { return (val - it) * dir > 0; }
+    void operator++() { it += step; }
+    const T& operator*() const { return it; }
+    ll sum() const {
+        ll a = it;
+        ll b = (stop - dir - it) / step * step + it;
+        ll n = (b - a) / step + 1;
+        return n > 0 ? (a + b) * n / 2 : 0;
+    }
+
+    friend Range reversed(const Range& r) {
+        auto new_start = (r.stop - r.dir - r.it) / r.step * r.step + r.it;
+        return {new_start, r.it - r.dir, -r.step};
+    }
+};
+//! @brief range(stop)
+template <class T>
+Range<T> irange(T stop) {
+    return Range(stop);
+}
+//! @brief range(start, stop[, step])
+template <class T>
+Range<T> irange(T start, T stop, T step = 1) {
+    return Range(start, stop, step);
+}
+}  // namespace bys
+
+namespace bys {
+void Solver::solve() {
+    auto [a, b, d] = scanner.read<ld, 3>();
+    geo::Point<ld> p(a, b);
+    print(p.rotate(geo::radian(d)));
+}
+}  // namespace bys
+
+int main() { return bys::Solver::main(/* bys::scanner.read<int>() */); }
