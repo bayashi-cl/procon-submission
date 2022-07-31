16ea52e8bd94d75c484de5511057113d4ef03174
Masaki Kobayashi <bayashi.cl@gmail.com>
2022-07-31 19:56:34 +0900

    abc261_e

---
+++ atcoder/abc261/abc261_e/33656026.nim
@@ -0,0 +1,165 @@
+static:
+  when not defined(lazyCompile):
+    template writeModule(path: string, code: untyped): untyped =
+      discard staticExec("mkdir -p $(dirname " & path & ")")
+      discard staticExec("cat - > " & path, astToStr(code))
+
+    writeModule("byslib/core/intops.nim"):
+      template `|`*(a, b: SomeInteger): untyped = a or b
+      template `&`*(a, b: SomeInteger): untyped = a and b
+      template `%`*(a, b: SomeInteger): untyped = a mod b
+      template `//`*(a, b: SomeInteger): untyped = a div b
+      template `<<`*(a, b: SomeInteger): untyped = a shl b
+      template `>>`*(a, b: SomeInteger): untyped = a shr b
+      
+      template `|=`*(a, b: SomeInteger): untyped = a = a or b
+      template `&=`*(a, b: SomeInteger): untyped = a = a and b
+      template `%=`*(a, b: SomeInteger): untyped = a = a mod b
+      template `//=`*(a, b: SomeInteger): untyped = a = a div b
+      template `<<=`*(a, b: SomeInteger): untyped = a = a shl b
+      template `>>=`*(a, b: SomeInteger): untyped = a = a shr b
+      
+      template `//+`*[T: SomeInteger](a, b: T): untyped =
+        if (a < 0) xor (b < 0):
+          a div b
+        else:
+          if a < 0:
+            (a + b + 1) div b
+          else:
+            (a + b - 1) div b
+      
+      template `//-`*[T: SomeInteger](a, b: T): untyped =
+        if (a < 0) xor (b < 0):
+          if a < 0:
+            (a - b + 1) div b
+          else:
+            (a - b - 1) div b
+        else:
+          a div b
+      
+
+    writeModule("byslib/core/utils.nim"):
+      template times*(n: int, body: untyped): untyped =
+        for _ in 0..<n:
+          body
+      
+      proc chmax*[T](a: var T; b: T): bool {.discardable.} =
+        if a < b:
+          a = b
+          result = true
+        else:
+          result = false
+      
+      proc chmin*[T](a: var T; b: T): bool {.discardable.} =
+        if a > b:
+          a = b
+          result = true
+        else:
+          result = false
+      
+
+    writeModule("byslib/core/io.nim"):
+      import macros, strutils, sequtils
+      
+      proc print*[Ty](args: varargs[Ty, `$`]) =
+        # 要検討
+        stdout.write(args.join(" "))
+        stdout.write('\n')
+      
+      let readNext* = iterator(getChar: bool = false): string {.closure.} =
+        while not stdin.endOfFile():
+          for bufStr in stdin.readLine.splitWhitespace():
+            block remainingBuf:
+              var cursor = 0
+              let bufSize = bufStr.len
+      
+              while getChar:
+                yield bufStr[cursor..cursor]
+                cursor.inc
+                if cursor == bufSize:
+                  break remainingBuf
+              if cursor == 0:
+                yield bufStr
+              else:
+                yield bufStr[cursor..bufStr.high]
+      
+      template read*(t: typedesc[string]): string = readNext()
+      template read*(t: typedesc[char]): char = readNext(true)[0]
+      template read*(t: typedesc[int]): t = readNext().parseInt()
+      template read*(t: typedesc[float]): t = readNext().parseFloat()
+      proc read*(t: typedesc[tuple]): t =
+        for it in result.fields():
+          it = read(it.type)
+      proc read*(n: static[int], t: typedesc): array[n, t] =
+        for i in 0..<n:
+          result[i] = read(t)
+      template readSeq*(n: int, t: typedesc): seq[t] =
+        newSeqWith(n, read(t))
+      template readSeq*(n, m: int, t: typedesc): seq[seq[t]] =
+        newSeqWith(n, newSeqWith(m, read(t)))
+      
+
+    writeModule("byslib/core/constant.nim"):
+      const Mod* = 998244353
+      const Mod7* = 1000000007
+      
+      template inf*(T): untyped =
+        when T is SomeFloat: T(Inf)
+        elif T is SomeInteger: T.high div 2
+        else: assert(false)
+      const IInf* = inf(int32)
+      const LInf* = inf(BiggestInt)
+      
+
+    writeModule("byslib/core.nim"):
+      {.warning[UnusedImport]: off.}
+      import std/[
+        macros, sugar, math, bitops,
+        sequtils, strutils, strformat, algorithm,
+        sets, tables, deques
+      ]
+      import core/[intops, utils, io, constant]
+      
+
+    type CompileError = object of CatchableError
+    let resp = gorgeEx("nim cpp -d:lazyCompile -p:. -d:release --opt:speed --multimethods:on --warning[SmallLshouldNotBeUsed]:off --hints:off --out:a.out Main.nim")
+    if resp.exitCode != 0:
+      raise newException(CompileError, resp.output)
+    quit(resp.exitCode)
+
+include byslib/core
+import bitops
+
+proc main =
+  type Query = range[1..3]
+  let n, c = read(int)
+  var
+    x = c
+    dp = newSeqWith(32, [0, 1])
+
+  n.times:
+    var xx = 0
+    let (t, a) = read((Query, int))
+
+    for d in 0..<32:
+      let b = a.testBit(d).int
+      case t
+      of 1:
+        dp[d][0] &= b
+        dp[d][1] &= b
+      of 2:
+        dp[d][0] |= b
+        dp[d][1] |= b
+      of 3:
+        dp[d][0] = dp[d][0] xor b
+        dp[d][1] = dp[d][1] xor b
+
+      if dp[d][x.testBit(d).int] == 1:
+        xx.setBit(d)
+
+    x = xx
+    print(x)
+
+
+
+if isMainModule: main()
