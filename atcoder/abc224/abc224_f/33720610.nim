cbc49e540aa48a018e13e6879ff91086492a6e17
Masaki Kobayashi <bayashi.cl@gmail.com>
2022-08-02 09:55:25 +0900

    abc224_f

---
+++ atcoder/abc224/abc224_f/33720610.nim
@@ -0,0 +1,192 @@
+static:
+  when not defined(lazyCompile):
+    template writeModule(path: string, code: untyped): untyped =
+      discard staticExec("mkdir -p $(dirname " & path & ")")
+      discard staticExec("cat - > " & path, astToStr(code))
+
+    writeModule("byslib/core/intops.nim"):
+      template `|`*(a, b: SomeInteger): untyped = a or b
+      template `&`*(a, b: SomeInteger): untyped = a and b
+      template `%`*(a, b: SomeInteger): untyped = a mod b
+      template `//`*(a, b: SomeInteger): untyped = a div b
+      template `<<`*(a, b: SomeInteger): untyped = a shl b
+      template `>>`*(a, b: SomeInteger): untyped = a shr b
+      
+      template `|=`*(a, b: SomeInteger): untyped = a = a or b
+      template `&=`*(a, b: SomeInteger): untyped = a = a and b
+      template `%=`*(a, b: SomeInteger): untyped = a = a mod b
+      template `//=`*(a, b: SomeInteger): untyped = a = a div b
+      template `<<=`*(a, b: SomeInteger): untyped = a = a shl b
+      template `>>=`*(a, b: SomeInteger): untyped = a = a shr b
+      
+      template `//+`*[T: SomeInteger](a, b: T): untyped =
+        if (a < 0) xor (b < 0):
+          a div b
+        else:
+          if a < 0:
+            (a + b + 1) div b
+          else:
+            (a + b - 1) div b
+      
+      template `//-`*[T: SomeInteger](a, b: T): untyped =
+        if (a < 0) xor (b < 0):
+          if a < 0:
+            (a - b + 1) div b
+          else:
+            (a - b - 1) div b
+        else:
+          a div b
+      
+
+    writeModule("byslib/core/utils.nim"):
+      template irange*(stop: SomeInteger): untyped = 0..<stop
+      template irange*(start, stop: SomeInteger): untyped = start..<stop
+      template times*(n: int, body: untyped): untyped =
+        for _ in 0..<n:
+          body
+      
+      template tern*[T](cond: bool, t, f: T): auto =
+        if cond: t
+        else: f
+      
+      proc chmax*[T](a: var T; b: T): bool {.discardable.} =
+        if a < b:
+          a = b
+          result = true
+        else:
+          result = false
+      
+      proc chmin*[T](a: var T; b: T): bool {.discardable.} =
+        if a > b:
+          a = b
+          result = true
+        else:
+          result = false
+      
+
+    writeModule("byslib/core/io.nim"):
+      import macros, strutils, sequtils
+      
+      template dumpImpl*[T](x: T): auto {.used.} =
+        when T is string or T is cstring:
+          x
+        elif compiles(for _ in x: discard):
+          x.join(" ")
+        else:
+          $x
+      
+      template print*[T](args: varargs[T, dumpImpl]) =
+        stdout.write(args.join(" "))
+        stdout.write('\n')
+      
+      when defined(release):
+        template debug*[T](args: varargs[T, dumpImpl]) = discard
+      else:
+        template debug*[T](args: varargs[T, dumpImpl]) =
+          let line = instantiationInfo().line
+          stderr.write("📌 line" & align($line, 4) & ": ")
+          stderr.write(args.join(" "))
+          stderr.write('\n')
+      
+      let readNext* = iterator(getChar: bool = false): string {.closure.} =
+        while not stdin.endOfFile():
+          for bufStr in stdin.readLine.splitWhitespace():
+            block remainingBuf:
+              var cursor = 0
+              let bufSize = bufStr.len
+      
+              while getChar:
+                yield bufStr[cursor..cursor]
+                cursor.inc
+                if cursor == bufSize:
+                  break remainingBuf
+              if cursor == 0:
+                yield bufStr
+              else:
+                yield bufStr[cursor..bufStr.high]
+      
+      template read*(t: typedesc[string]): string = readNext()
+      template read*(t: typedesc[char]): char = readNext(true)[0]
+      template read*(t: typedesc[int]): t = readNext().parseInt()
+      template read*(t: typedesc[float]): t = readNext().parseFloat()
+      proc read*(t: typedesc[tuple]): t =
+        for it in result.fields():
+          it = read(it.type)
+      proc read*(n: static[int], t: typedesc): array[n, t] =
+        for i in 0..<n:
+          result[i] = read(t)
+      template readSeq*(n: int, t: typedesc): seq[t] =
+        newSeqWith(n, read(t))
+      template readSeq*(n, m: int, t: typedesc): auto =
+        newSeqWith(n, newSeqWith(m, read(t)))
+      
+
+    writeModule("byslib/core/constant.nim"):
+      const Mod* = 998244353
+      const Mod7* = 1000000007
+      
+      template inf*(T): untyped =
+        when T is SomeFloat: T(Inf)
+        elif T is SomeInteger: T.high div 2
+        else: assert(false)
+      const IInf* = inf(int32)
+      const LInf* = inf(BiggestInt)
+      
+
+    writeModule("byslib/core.nim"):
+      {.warning[UnusedImport]: off.}
+      import std/[
+        macros, sugar, math, bitops,
+        sequtils, strutils, strformat, algorithm,
+        sets, tables, deques
+      ]
+      import core/[intops, utils, io, constant]
+      
+
+    type CompileError = object of CatchableError
+    let resp = gorgeEx("nim cpp -d:lazyCompile -p:. -d:release --opt:speed --multimethods:on --warning[SmallLshouldNotBeUsed]:off --hints:off --out:a.out Main.nim")
+    if resp.exitCode != 0:
+      raise newException(CompileError, resp.output)
+    quit(resp.exitCode)
+
+include byslib/core
+
+proc powMod*(p, q, m: int): int =
+  if q == 0: return 1
+  result = 1
+  var base = p
+  for d in 0..fastLog2(q):
+    if q.testBit(d):
+      result = result * base mod m
+    base = base * base mod m
+
+proc main =
+  const zero = int('0')
+  let
+    s = read(string)
+    t = s.mapIt(it.int - zero)
+    n = t.len
+
+  var
+    twos = newSeq[int](n + 1)
+    fives = newSeq[int](n + 1)
+    tens = newSeq[int](n + 1)
+    sigma = newSeq[int](n + 1)
+
+  twos[0] = 1
+  fives[0] = 1
+  tens[0] = 1
+  sigma[0] = 1
+  for i in 1..n:
+    twos[i] = twos[i - 1] * 2 % Mod
+    fives[i] = fives[i - 1] * 5 % Mod
+    tens[i] = tens[i - 1] * 10 % Mod
+    sigma[i] = (sigma[i - 1] + fives[i]) % Mod
+
+  var ans = t[^1] * twos[n - 1]
+  for i in 0..n-2:
+    let r = n - i - 2
+    ans += (twos[r] * sigma[r] + tens[r + 1]) % Mod * (twos[i] * t[i]) % Mod
+  print(ans % Mod)
+
+if isMainModule: main()
