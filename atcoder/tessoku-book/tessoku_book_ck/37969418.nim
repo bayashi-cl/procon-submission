99265b0676684ae2fe6ff8f6a90810dcd0a2f87c
Masaki Kobayashi <bayashi.cl@gmail.com>
2023-01-13 07:51:00 +0900

    tessoku_book_ck

---
+++ atcoder/tessoku-book/tessoku_book_ck/37969418.nim
@@ -0,0 +1,185 @@
+static:
+  when not defined(lazyCompile):
+    template writeModule(path: string, code: untyped): untyped =
+      discard staticExec("mkdir -p $(dirname " & path & ")")
+      discard staticExec("cat - > " & path, astToStr(code))
+
+    writeModule("byslib/core/intops.nim"):
+      import math
+      
+      template `|`*(a, b: SomeInteger): untyped = a or b
+      template `&`*(a, b: SomeInteger): untyped = a and b
+      template `%`*(a, b: SomeInteger): untyped = floorMod(a, b)
+      template `//`*(a, b: SomeInteger): untyped = floorDiv(a, b)
+      template `<<`*(a, b: SomeInteger): untyped = a shl b
+      template `>>`*(a, b: SomeInteger): untyped = a shr b
+      
+      template `|=`*(a, b: SomeInteger): untyped = a = a or b
+      template `&=`*(a, b: SomeInteger): untyped = a = a and b
+      template `%=`*(a, b: SomeInteger): untyped = a = floorMod(a, b)
+      template `//=`*(a, b: SomeInteger): untyped = a = floorDiv(a, b)
+      template `<<=`*(a, b: SomeInteger): untyped = a = a shl b
+      template `>>=`*(a, b: SomeInteger): untyped = a = a shr b
+      
+      template `//+`*[T: SomeInteger](a, b: T): untyped = ceilDiv(a, b)
+      
+
+    writeModule("byslib/core/utils.nim"):
+      template times*(n: int, body: untyped): untyped =
+        for _ in 0..<n:
+          body
+      
+      template tern*[T](cond: bool, t, f: T): auto =
+        if cond: t
+        else: f
+      
+      proc chmax*[T](a: var T; b: T): bool {.discardable.} =
+        if a < b:
+          a = b
+          result = true
+        else:
+          result = false
+      
+      proc chmin*[T](a: var T; b: T): bool {.discardable.} =
+        if a > b:
+          a = b
+          result = true
+        else:
+          result = false
+      
+
+    writeModule("byslib/core/io.nim"):
+      import macros, strutils, sequtils
+      
+      template proconDollars*[T](x: T): auto =
+        when T is string or T is cstring:
+          x
+        elif compiles(x.join):
+          x.join(" ")
+        elif compiles(x.toSeq):
+          x.toSeq.join(" ")
+        else:
+          $x
+      
+      template print*(args: varargs[string, proconDollars]) =
+        stdout.write(args.join(" "))
+        stdout.write('\n')
+      
+      
+      when defined(release):
+        template debug*(args: varargs[string, `$`]) = discard
+      else:
+        template debug*(args: varargs[string, `$`]) =
+          let line = instantiationInfo().line
+          stderr.write("📌 line" & align($line, 4) & ": ")
+          stderr.write(args.join(" "))
+          stderr.write('\n')
+      
+      let readNext* = iterator(getChar: bool = false): string {.closure.} =
+        while not stdin.endOfFile():
+          for bufStr in stdin.readLine.splitWhitespace():
+            block remainingBuf:
+              var cursor = 0
+              let bufSize = bufStr.len
+      
+              while getChar:
+                yield bufStr[cursor..cursor]
+                cursor.inc
+                if cursor == bufSize:
+                  break remainingBuf
+              if cursor == 0:
+                yield bufStr
+              else:
+                yield bufStr[cursor..bufStr.high]
+      
+      template read*(t: typedesc[string]): string = readNext()
+      template read*(t: typedesc[char]): char = readNext(true)[0]
+      template read*(t: typedesc[int]): t = readNext().parseInt()
+      template read*(t: typedesc[float]): t = readNext().parseFloat()
+      proc read*(t: typedesc[tuple]): t =
+        for it in result.fields():
+          it = read(it.type)
+      proc read*(n: static[int], t: typedesc): array[n, t] =
+        for i in 0..<n:
+          result[i] = read(t)
+      template readSeq*(n: int, t: typedesc): seq[t] =
+        newSeqWith(n, read(t))
+      template readSeq*(n, m: int, t: typedesc): auto =
+        newSeqWith(n, newSeqWith(m, read(t)))
+      
+
+    writeModule("byslib/core/constant.nim"):
+      const Mod* = 998244353
+      const Mod7* = 1000000007
+      
+      template inf*(T): untyped =
+        when T is SomeFloat: T(Inf)
+        elif T is SomeInteger: T.high div 2
+        else: assert(false)
+      const IInf* = inf(int32)
+      const LInf* = inf(BiggestInt)
+      
+
+    writeModule("byslib/algo/bisect.nim"):
+      import sugar
+      import ../core/utils
+      
+      proc meguru_bisect*(ok, ng: int, check: (int) -> bool): int =
+        assert check(ok)
+        assert not check(ng)
+      
+        var
+          okv = ok
+          ngv = ng
+      
+        while abs(okv - ngv) > 1:
+          let mid = (okv + ngv) div 2
+          if check(mid):
+            okv = mid
+          else:
+            ngv = mid
+      
+        return okv
+      
+      proc bisect_float*(ok, ng: float, check: (float) -> bool, rep = 100): float =
+        assert check(ok)
+        assert not check(ng)
+      
+        var
+          okv = ok
+          ngv = ng
+      
+        rep.times:
+          let mid = (okv + ngv) / 2
+          if check(mid):
+            okv = mid
+          else:
+            ngv = mid
+      
+        return okv
+      
+
+    writeModule("byslib/core.nim"):
+      {.warning[UnusedImport]: off.}
+      import std/[
+        macros, sugar, math, bitops,
+        sequtils, strutils, strformat, algorithm,
+        sets, tables, deques
+      ]
+      import core/[intops, utils, io, constant]
+      
+
+    type CompileError = object of CatchableError
+    let resp = gorgeEx("nim cpp -d:lazyCompile -p:. -d:release --opt:speed --multimethods:on --warning[SmallLshouldNotBeUsed]:off --hints:off --out:a.out Main.nim")
+    if resp.exitCode != 0:
+      raise newException(CompileError, resp.output)
+    quit(resp.exitCode)
+
+include byslib/core
+import byslib/algo/bisect
+
+proc main =
+  let n = read(float)
+  print(bisect_float(0.0, n, x => x^3+x <= n))
+
+if isMainModule: main()
