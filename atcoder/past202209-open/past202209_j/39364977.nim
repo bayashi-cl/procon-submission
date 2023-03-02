016f41937a45b467311f202610a4958596727e5e
Masaki Kobayashi <bayashi.cl@gmail.com>
2023-03-03 08:41:00 +0900

    past202209_j

---
+++ atcoder/past202209-open/past202209_j/39364977.nim
@@ -0,0 +1,185 @@
+static:
+  when not defined(lazyCompile):
+    template writeModule(path: string, code: untyped): untyped =
+      discard staticExec("mkdir -p $(dirname " & path & ")")
+      discard staticExec("cat - > " & path, astToStr(code))
+
+    writeModule("byslib/core/intops.nim"):
+      import math
+      
+      template `|`*(a, b: SomeInteger): untyped = a or b
+      template `&`*(a, b: SomeInteger): untyped = a and b
+      template `%`*(a, b: SomeInteger): untyped = floorMod(a, b)
+      template `//`*(a, b: SomeInteger): untyped = floorDiv(a, b)
+      template `<<`*(a, b: SomeInteger): untyped = a shl b
+      template `>>`*(a, b: SomeInteger): untyped = a shr b
+      
+      template `|=`*(a, b: SomeInteger): untyped = a = a or b
+      template `&=`*(a, b: SomeInteger): untyped = a = a and b
+      template `%=`*(a, b: SomeInteger): untyped = a = floorMod(a, b)
+      template `//=`*(a, b: SomeInteger): untyped = a = floorDiv(a, b)
+      template `<<=`*(a, b: SomeInteger): untyped = a = a shl b
+      template `>>=`*(a, b: SomeInteger): untyped = a = a shr b
+      
+      template `//+`*[T: SomeInteger](a, b: T): untyped = ceilDiv(a, b)
+      
+
+    writeModule("byslib/core/utils.nim"):
+      template times*(n: int, body: untyped): untyped =
+        for _ in 0..<n:
+          body
+      
+      template tern*[T](cond: bool, t, f: T): auto =
+        if cond: t
+        else: f
+      
+      proc chmax*[T](a: var T; b: T): bool {.discardable.} =
+        if a < b:
+          a = b
+          result = true
+        else:
+          result = false
+      
+      proc chmin*[T](a: var T; b: T): bool {.discardable.} =
+        if a > b:
+          a = b
+          result = true
+        else:
+          result = false
+      
+
+    writeModule("byslib/core/io.nim"):
+      import macros, strutils, sequtils
+      
+      template proconDollars*[T](x: T): auto =
+        when T is string or T is cstring:
+          x
+        elif compiles(x.join):
+          x.join(" ")
+        elif compiles(x.toSeq):
+          x.toSeq.join(" ")
+        else:
+          $x
+      
+      template print*(args: varargs[string, proconDollars]) =
+        stdout.write(args.join(" "))
+        stdout.write('\n')
+      
+      when defined(release):
+        template debug*(args: varargs[string, `$`]) = discard
+      else:
+        template debug*(args: varargs[string, `$`]) =
+          let line = instantiationInfo().line
+          stderr.write("📌 line" & align($line, 4) & ": ")
+          stderr.write(args.join(" "))
+          stderr.write('\n')
+      
+
+    writeModule("byslib/core/read.nim"):
+      ## Input
+      
+      runnableExamples:
+        let
+          n = read(int)
+          h, w = read(int) - 1
+          c = readSeq(h, w, int)
+          p = readSeq(n, tuple[x, y: int])
+      
+      import macros, strutils, sequtils
+      
+      let readNext* = iterator: string {.closure.} =
+        while not stdin.endOfFile():
+          for str in stdin.readLine.splitWhitespace():
+            yield str
+      
+      template read*(tp: typedesc): auto =
+        when tp is string:
+          readNext()
+        elif tp is char:
+          readNext()[0]
+        elif tp is int:
+          readNext().parseInt()
+        elif tp is float:
+          readNext().parseFloat()
+        elif tp is tuple:
+          var result: tp
+          for it in result.fields(): it = read(it.type)
+          result
+        elif tp is array:
+          var result: tp
+          for i in result.low..result.high: result[i] = read(int)
+          result
+        else:
+          doAssert: false
+      
+      template readSeq*(n: int; t: typedesc): auto = newSeqWith(n, read(t))
+      template readSeq*(n, m: int; t: typedesc): auto = newSeqWith(n, newSeqWith(m, read(t)))
+      
+
+    writeModule("byslib/core/constant.nim"):
+      const Mod* = 998244353
+      const Mod7* = 1000000007
+      
+      template inf*(T): untyped =
+        when T is SomeFloat: T(Inf)
+        elif T is SomeInteger: T.high div 2
+        else: assert(false)
+      const IInf* = inf(int)
+      
+
+    writeModule("byslib/core.nim"):
+      {.warning[UnusedImport]: off.}
+      import std/[
+        macros, sugar, math, bitops,
+        sequtils, strutils, strformat, algorithm,
+        sets, tables, deques
+      ]
+      import core/[intops, utils, io, read, constant]
+      
+
+    type CompileError = object of CatchableError
+    let resp = gorgeEx("nim cpp -d:lazyCompile -p:. -d:release --opt:speed --multimethods:on --warning[SmallLshouldNotBeUsed]:off --hints:off --out:a.out Main.nim")
+    if resp.exitCode != 0:
+      raise newException(CompileError, resp.output)
+    quit(resp.exitCode)
+
+include byslib/core
+
+proc tipArea(r, theta: float): float =
+  let
+    c = r^2 * PI * (theta / (2*PI))
+    t = (r^2) * sin(theta) / 2
+
+  return c - t
+
+proc main =
+  var h, w = read(int)
+  let d = read(int)
+  if h > w: swap(h, w)
+
+  let
+    circle = (d^2).toFloat * PI
+    rec = (h * w).toFloat
+  var ans: float
+
+  if h < d * 2:
+    let l = sqrt((d^2).toFloat - (h / 2)^2) * 2
+    if w.toFloat < l:
+      ans = 1.0
+    else:
+      var remCircle = circle
+      let thetaH = ((h/2) / d.toFloat).arccos * 2
+      remCircle -= tipArea(d.toFloat, thetaH) * 2
+
+      if w < d * 2:
+        let thetaW = ((w/2) / d.toFloat).arccos * 2
+        remCircle -= tipArea(d.toFloat, thetaW) * 2
+
+      ans = remCircle / rec
+
+  else:
+    ans = circle / rec
+
+  print(ans.formatFloat(ffDecimal))
+
+if isMainModule: main()
